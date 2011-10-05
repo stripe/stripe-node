f9d307c4518c2dcf19038b852e263d40e2ad11a7
Ask Bjørn Hansen <ask@develooper.com>
2011-10-05 11:41:15 -0700

    Untabify

--- test/charges.js
+++ test/charges.js
@@ -12,53 +12,53 @@ if (!api_key) {
 var stripe = require('./../lib/main.js')(api_key);
 
 vows.describe("Charges API").addBatch({
-	'Create charge' : {
-		topic: function() {
-			stripe.charges.create({
-				amount: "50",
-				currency: "usd",
-				card: { number: "4242424242424242",
-					exp_month: 12,
-					exp_year:  2020,
-					name: "T. Ester",
-				}
-			}, this.callback);
-		},
-		'returns a charge' : function(err, response) {
-			assert.isNull(err);
-			console.log("response", response);
-			assert.isDefined(response);
-			assert.equal(response.object, 'charge');
-			assert.isDefined(response.id);
-		},
-		'Retrieve a charge' : {
-			topic: function(create_err, charge) {
-				stripe.charges.retrieve(charge.id, this.callback);
-			},
-			'Got a charge' : function(err, response) {
-				assert.isNull(err);
-				assert.isDefined(response);
-				assert.equal(response.object, 'charge');
-				assert.isDefined(response.id);
-			},
-		},
-		'Refund a charge' : {
-			topic: function(create_err, charge) {
-				stripe.charges.refund(charge.id, 50, this.callback);
-			},
-			'Got a refund' : function(err, response) {
-				assert.isNull(err);
-				assert.isDefined(response);
-				assert.isTrue(response.refunded);
-			},
-		},
-	},
-	'Charge list' : {
-		topic: function() {
-			stripe.charges.list({}, this.callback);
-		},
-		'Got count': function(err, response) {
-			assert.isNumber(response.count);
-		},
-	}
+    'Create charge' : {
+        topic: function() {
+            stripe.charges.create({
+                amount: "50",
+                currency: "usd",
+                card: { number: "4242424242424242",
+                    exp_month: 12,
+                    exp_year:  2020,
+                    name: "T. Ester",
+                }
+            }, this.callback);
+        },
+        'returns a charge' : function(err, response) {
+            assert.isNull(err);
+            console.log("response", response);
+            assert.isDefined(response);
+            assert.equal(response.object, 'charge');
+            assert.isDefined(response.id);
+        },
+        'Retrieve a charge' : {
+            topic: function(create_err, charge) {
+                stripe.charges.retrieve(charge.id, this.callback);
+            },
+            'Got a charge' : function(err, response) {
+                assert.isNull(err);
+                assert.isDefined(response);
+                assert.equal(response.object, 'charge');
+                assert.isDefined(response.id);
+            },
+        },
+        'Refund a charge' : {
+            topic: function(create_err, charge) {
+                stripe.charges.refund(charge.id, 50, this.callback);
+            },
+            'Got a refund' : function(err, response) {
+                assert.isNull(err);
+                assert.isDefined(response);
+                assert.isTrue(response.refunded);
+            },
+        },
+    },
+    'Charge list' : {
+        topic: function() {
+            stripe.charges.list({}, this.callback);
+        },
+        'Got count': function(err, response) {
+            assert.isNumber(response.count);
+        },
+    }
 }).export(module, {error: false});
