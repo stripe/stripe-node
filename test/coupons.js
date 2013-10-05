422612780baf9a5bdd977ed4e8c95bf11fdb9427
David Rivera <david.r.rivera193@gmail.com>
2013-10-05 09:19:40 -0400

    updated coupons api and added test cases for it

---
+++ test/coupons.js
@@ -0,0 +1,62 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    sys = require('sys');
+
+var api_key = process.env.STRIPE_API;
+
+if (!api_key) {
+    sys.puts('To run vows, you must have a STRIPE_API environment variable with a test api key');
+    process.exit(2)
+}
+
+var stripe = require('./../lib/main.js')(api_key);
+
+var coupon;
+
+vows.describe("Coupon API").addBatch({
+        'Create a coupon': {
+            topic: function() {
+                stripe.coupons.create({id: "coupon4", 
+                                       duration: "forever", 
+                                       percent_off:50}, this.callback);
+            },
+            'created coupon': function(err, response) {
+                assert.isNull(err);
+
+                assert.isDefined(response);
+                assert.equal(response.id, "coupon4");
+                assert.equal(response.duration, "forever"); 
+            },
+            'Retrieve a coupon': {
+                topic: function(cou, coupon) {
+                    stripe.coupons.retrieve(coupon.id, this.callback); 
+                },
+                'got a coupon': function(err, response) {
+                    assert.isNull(err);
+                    
+                    assert.isDefined(response);
+                    assert.equal(response.id, 'coupon4');
+
+                    coupon = response; 
+                }
+            }, 
+            'List coupons': {
+                topic: function(cou, coupon) {
+                    stripe.coupons.list(0, 10, this.callback); 
+                }, 
+                'Got coupons': function(err, response) {
+                    assert.isNull(err); 
+                    assert.isNumber(response.count); 
+                }, 
+                'Delete coupon': {
+                    topic: function() {
+                        stripe.coupons.del(coupon.id, this.callback); 
+                    }, 
+                    'Coupon deleted': function(err, response) {
+                        assert.isNull(err); 
+                        assert.isTrue(response.deleted)
+                    }
+                }
+            } 
+        }
+    }).export(module, {error: false});
