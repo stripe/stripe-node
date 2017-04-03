b6dc451fd122c931aad35969c284e7b61bf4640c
Brandur <brandur@mutelight.org>
2017-04-03 12:21:14 -0700

    Improve error message when ommitting a parameter
    Improve the error message when a `stripeMethod` is called but a
    parameter is accidentally ommitted. Previously, we were accidentally
    including the product of `utils.makeURLInterpolator` in the error
    string, which made it very difficult to read:
    
    ```
    Error: Stripe: Argument "id" required, but got: undefined (on API request to GET function (outputs) {
            return cleanString.replace(/\{([\s\S]+?)\}/g, function($0, $1) {
              return encodeURIComponent(outputs[$1] || '');
            });
          })
        at Object.<anonymous> (/Users/brandur/stripe/stripe-node/lib/StripeMethod.js:53:21)
    ```
    
    I think this was basically an implementation accident, as what it's
    actually trying to do is print the path of the API call that's about to
    be invoked. We now produce this instead:
    
    ```
    Error: Stripe: Argument "id" required, but got: undefined (on API request to `GET /invoices/{id}`)
        at Object.<anonymous> (/Users/brandur/stripe/stripe-node/lib/StripeMethod.js:53:21)
    ```
    
    Fixes #323.

---
+++ test/StripeResource.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('./testUtils');
+
+var stripe = require('./testUtils').getSpyableStripe();
+var expect = require('chai').expect;
+
+describe('StripeResource', function() {
+  describe('createResourcePathWithSymbols', function() {
+    it('Generates a path', function() {
+      var invoice = stripe.invoices.create({});
+      var path = stripe.invoices.createResourcePathWithSymbols('{id}');
+      expect(path).to.equal('/invoices/{id}');
+    });
+  });
+});
