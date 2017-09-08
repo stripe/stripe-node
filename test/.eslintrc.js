1c7d1cd34928c873ca6312a9ef45ea7b02785ab0
Olivier Bellone <ob@stripe.com>
2017-09-08 15:24:18 +0200

    Use ESLint instead of jscs

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
         "chai-friendly"
     ],
     "rules": {
-        "no-loop-func": "off",
-        "no-sync": "off",
         "no-unused-expressions": 0,
         "chai-friendly/no-unused-expressions": 2
     }
