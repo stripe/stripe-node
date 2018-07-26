36deea92cdf9cdfd572b2e92e111ec894f845d7d
Jonathan Lomas <jlomas@stripe.com>
2018-07-26 10:48:11 -0700

    Add Issuing API calls, create a 'StripeNamespace' to accommmodate the structure

---
+++ test/resources/Issuing/Authorization.spec.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var stripe = require('../../testUtils').getSpyableStripe();
+var expect = require('chai').expect;
+
+describe('Issuing', function () {
+  describe('Authorization Resource', function() {
+    describe('retrieve', function() {
+      it('Sends the correct request', function() {
+        stripe.issuing.authorizations.retrieve('iauth_123');
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/issuing/authorizations/iauth_123',
+          data: {},
+          headers: {},
+        });
+      });
+    });
+
+    describe('list', function() {
+      it('Sends the correct request', function() {
+        stripe.issuing.authorizations.list();
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/issuing/authorizations',
+          data: {},
+          headers: {},
+        });
+      });
+    });
+
+    describe('update', function() {
+      it('Sends the correct request', function() {
+        stripe.issuing.authorizations.update('iauth_123', {
+          metadata: {
+            thing1: true,
+            thing2: 'yes'
+          },
+        });
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'POST',
+          url: '/v1/issuing/authorizations/iauth_123',
+          headers: {},
+          data: {
+            metadata: {
+              thing1: true,
+              thing2: 'yes'
+            },
+          },
+        });
+      });
+    });
+
+    describe('approve', function() {
+      it('Sends the correct request', function() {
+        stripe.issuing.authorizations.approve('iauth_123');
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'POST',
+          url: '/v1/issuing/authorizations/iauth_123/approve',
+          headers: {},
+          data: {},
+        });
+      });
+    });
+
+    describe('decline', function() {
+      it('Sends the correct request', function() {
+        stripe.issuing.authorizations.decline('iauth_123');
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'POST',
+          url: '/v1/issuing/authorizations/iauth_123/decline',
+          headers: {},
+          data: {},
+        });
+      });
+    });
+  });
+});
