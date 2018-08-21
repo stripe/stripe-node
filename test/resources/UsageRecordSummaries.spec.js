1f4eaa3be206bdc54d5a784a9da1c5dddf3e1d4b
Remi Jannel <remi@stripe.com>
2018-08-21 08:18:59 -0400

    Add support for usage record summaries

---
+++ test/resources/UsageRecordSummaries.spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var stripe = require('../testUtils').getSpyableStripe();
+var expect = require('chai').expect;
+
+describe('UsageRecordSummaries Resource', function() {
+  describe('list', function() {
+    it('Sends the correct request', function() {
+      stripe.usageRecordSummaries.list('si_123', {});
+
+      expect(stripe.LAST_REQUEST).to.deep.equal({
+        method: 'GET',
+        url: '/v1/subscription_items/si_123/usage_record_summaries',
+        headers: {},
+        data: {}
+      });
+    });
+
+    it('Includes any options that were provided', function(done) {
+      stripe.usageRecordSummaries.list('si_123', {}, {
+        stripe_account: 'acct_456',
+      }).then(function(record) {
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/subscription_items/si_123/usage_record_summaries',
+          headers: {
+            'Stripe-Account': 'acct_456'
+          },
+          data: {}
+        });
+
+        done();
+      });
+    });
+
+    it('Calls a given callback', function(done) {
+      stripe.usageRecordSummaries.list('si_123', {}, function(error, record) {
+        done(error);
+      });
+    });
+  });
+});
