6cc3f2f7f0557ef427e6ff45dc65032f23d10d87
David Zhu <daz@stripe.com>
2018-09-17 14:34:51 -0700

    Terminal Bindings

---
+++ test/resources/Terminal/Locations.spec.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var stripe = require('../../testUtils').getSpyableStripe();
+
+var expect = require('chai').expect;
+
+describe('Terminal', function () {
+  describe('Locations Resource', function () {
+    describe('retrieve', function () {
+      it('Sends the correct request', function () {
+        stripe.terminal.locations.retrieve('loc_123');
+
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/terminal/locations/loc_123',
+          headers: {},
+          data: {},
+        });
+      });
+    });
+
+    describe('create', function () {
+      it('Sends the correct request', function () {
+        stripe.terminal.locations.create({
+          display_name: 'name',
+          address: {
+            line1: 'line1',
+            country: 'US',
+            postal_code: '12345',
+            state: 'CA',
+            city: 'San Francisco',
+          },
+        });
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'POST',
+          url: '/v1/terminal/locations',
+          headers: {},
+          data: {
+            display_name: 'name',
+            address: {
+              line1: 'line1',
+              country: 'US',
+              postal_code: '12345',
+              state: 'CA',
+              city: 'San Francisco',
+            },
+          },
+        });
+      });
+    });
+
+    describe('update', function () {
+      it('Sends the correct request', function () {
+        stripe.terminal.locations.update('loc_123', {
+          display_name: 'name'
+        });
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'POST',
+          url: '/v1/terminal/locations/loc_123',
+          headers: {},
+          data: {
+            display_name: 'name',
+          },
+        });
+      });
+    });
+
+    describe('list', function () {
+      it('Sends the correct request', function () {
+        stripe.terminal.locations.list();
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/terminal/locations',
+          headers: {},
+          data: {},
+        });
+      });
+    });
+  });
+});
---
+++ test/resources/Terminal/Readers.spec.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var stripe = require('../../testUtils').getSpyableStripe();
+
+var expect = require('chai').expect;
+
+describe('Terminal', function () {
+  describe('Readers Resource', function () {
+    describe('retrieve', function () {
+      it('Sends the correct request', function () {
+        stripe.terminal.readers.retrieve('rdr_123');
+
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/terminal/readers/rdr_123',
+          headers: {},
+          data: {},
+        });
+      });
+    });
+
+    describe('create', function () {
+      it('Sends the correct request', function () {
+        stripe.terminal.readers.create({
+          registration_code: 'a-b-c',
+          label: 'name',
+        });
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'POST',
+          url: '/v1/terminal/readers',
+          headers: {},
+          data: {
+            registration_code: 'a-b-c',
+            label: 'name',
+          },
+        });
+      });
+    });
+
+    describe('update', function () {
+      it('Sends the correct request', function () {
+        stripe.terminal.readers.update('rdr_123', {
+          label: 'name'
+        });
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'POST',
+          url: '/v1/terminal/readers/rdr_123',
+          headers: {},
+          data: {
+            label: 'name',
+          },
+        });
+      });
+    });
+
+    describe('list', function () {
+      it('Sends the correct request', function () {
+        stripe.terminal.readers.list();
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/terminal/readers',
+          headers: {},
+          data: {},
+        });
+      });
+    });
+  });
+});
