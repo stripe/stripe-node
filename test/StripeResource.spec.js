572dae1d903f05ed3fe3d7a2369f4fa448500f53
Paul Asjes <paulasjes@stripe.com>
2019-01-29 13:03:34 -0800

    added _shouldRetry and rejigged nock logic

--- test/StripeResource.spec.js
+++ test/StripeResource.spec.js
@@ -35,30 +35,66 @@ describe('StripeResource', function() {
     });
   });
 
-  describe('_request', function() {
-    it('throws an error on connection failure', function() {
-      var stripe = require('../lib/stripe')(utils.getUserStripeKey());
+  describe('Retry Network Requests', function() {
+    // use a real instance of stripe as we're mocking the http.request responses
+    var realStripe = require('../lib/stripe')(utils.getUserStripeKey());
+    var options = {
+      host: stripe.getConstant('DEFAULT_HOST'),
+      path: '/v1/charges',
+      data: {
+        amount: 1000,
+        currency: 'usd',
+        source: 'tok_visa',
+        description: 'test'
+      }
+    };
 
-      var options = {
-        method: 'POST',
-        host: stripe.getConstant('DEFAULT_HOST'),
-        path: '/v1/charges',
-        data: {
-          amount: 1000,
-          currency: 'usd',
-          source: 'tok_visa',
-          description: 'test'
-        }
-      };
+    afterEach(function() {
+      nock.cleanAll();
+    });
 
+    describe('_request', function() {
+      // mock the 500
       nock('https://' + options.host)
-        .get(options.path)
-        .reply(500, {
-          error: 'foo errors'
+        .post(options.path, options.data)
+        .replyWithError({
+          type: 'StripeConnectionError'
         });
 
-      stripe.charges.create(options.data, function(err, charge) {
-        expect(err.message).to.equal('An error occurred with our connection to Stripe');
+      it('throws an error on connection failure', function(done) {
+        realStripe.charges.create(options.data, function(err, charge) {
+          expect(err.detail).to.deep.equal({
+            type: 'StripeConnectionError'
+          });
+
+          done();
+        });
+      });
+    });
+
+    describe('_shouldRetry', function() {
+      it('should return false if we\'ve reached maximum retries', function() {
+        var res = stripe.invoices._shouldRetry({}, 0);
+
+        expect(res).to.equal(false);
+      });
+
+      it('should return true if we have more retries available', function() {
+        stripe.setMaxNetworkRetries(1);
+        var res = stripe.invoices._shouldRetry({
+          type: 'StripeConnectionError'
+        }, 0);
+
+        expect(res).to.equal(true);
+      });
+
+      it('should return false if the error is not an API or connection error', function() {
+        stripe.setMaxNetworkRetries(1);
+        var res = stripe.invoices._shouldRetry({
+          type: 'foo'
+        }, 0);
+
+        expect(res).to.equal(false);
       });
     });
   });
