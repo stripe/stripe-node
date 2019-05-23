1b5eb16cc5a91abdb9a7ce65126bebe426c472b2
Olivier Bellone <ob@stripe.com>
2019-05-23 16:19:24 -0700

    Add support for radar.early_fraud_warning resource

---
+++ test/resources/Radar/EarlyFraudWarnings.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const stripe = require('../../../testUtils').getSpyableStripe();
+const expect = require('chai').expect;
+
+describe('Radar', () => {
+  describe('EarlyFraudWarnings Resource', () => {
+    describe('retrieve', () => {
+      it('Sends the correct request', () => {
+        stripe.radar.earlyFraudWarnings.retrieve('issfr_123');
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/radar/early_fraud_warnings/issfr_123',
+          data: {},
+          headers: {},
+        });
+      });
+    });
+
+    describe('list', () => {
+      it('Sends the correct request', () => {
+        stripe.radar.earlyFraudWarnings.list();
+        expect(stripe.LAST_REQUEST).to.deep.equal({
+          method: 'GET',
+          url: '/v1/radar/early_fraud_warnings',
+          data: {},
+          headers: {},
+        });
+      });
+    });
+  });
+});
