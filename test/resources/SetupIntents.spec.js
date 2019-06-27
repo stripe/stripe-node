44e9b545565ac6a8194ebcbef24e62717c380257
Remi Jannel <remi@stripe.com>
2019-06-27 16:48:07 -0700

    Add support for the `SetupIntent` resource and APIs

---
+++ test/resources/SetupIntents.spec.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const stripe = require('../../testUtils').getSpyableStripe();
+const expect = require('chai').expect;
+
+const SETUP_INTENT_TEST_ID = 'seti_123';
+
+describe('Setup Intents Resource', () => {
+  describe('create', () => {
+    it('Sends the correct request', () => {
+      const params = {
+        payment_method_types: ['card'],
+      };
+      stripe.setupIntents.create(params);
+      expect(stripe.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/v1/setup_intents',
+        headers: {},
+        data: params,
+      });
+    });
+  });
+
+  describe('list', () => {
+    it('Sends the correct request', () => {
+      stripe.setupIntents.list();
+      expect(stripe.LAST_REQUEST).to.deep.equal({
+        method: 'GET',
+        url: '/v1/setup_intents',
+        headers: {},
+        data: {},
+      });
+    });
+  });
+
+  describe('retrieve', () => {
+    it('Sends the correct request', () => {
+      stripe.setupIntents.retrieve(SETUP_INTENT_TEST_ID);
+      expect(stripe.LAST_REQUEST).to.deep.equal({
+        method: 'GET',
+        url: `/v1/setup_intents/${SETUP_INTENT_TEST_ID}`,
+        headers: {},
+        data: {},
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('Sends the correct request', () => {
+      stripe.setupIntents.update(SETUP_INTENT_TEST_ID, {
+        metadata: {key: 'value'},
+      });
+      expect(stripe.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: `/v1/setup_intents/${SETUP_INTENT_TEST_ID}`,
+        headers: {},
+        data: {metadata: {key: 'value'}},
+      });
+    });
+  });
+
+  describe('cancel', () => {
+    it('Sends the correct request', () => {
+      stripe.setupIntents.cancel(SETUP_INTENT_TEST_ID);
+      expect(stripe.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: `/v1/setup_intents/${SETUP_INTENT_TEST_ID}/cancel`,
+        headers: {},
+        data: {},
+      });
+    });
+  });
+
+  describe('confirm', () => {
+    it('Sends the correct request', () => {
+      stripe.setupIntents.confirm(SETUP_INTENT_TEST_ID);
+      expect(stripe.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: `/v1/setup_intents/${SETUP_INTENT_TEST_ID}/confirm`,
+        headers: {},
+        data: {},
+      });
+    });
+  });
+});
