d3b31ed6a7c299b7a01ae5d33919f522e0532680
John Bohannon <imjohnbo@github.com>
2019-09-28 12:05:03 -0400

    Fix lint

--- test/StripeResource.spec.js
+++ test/StripeResource.spec.js
@@ -64,9 +64,7 @@ describe('StripeResource', () => {
 
         const scope = nock(`https://${options.host}`)
           .get(
-            `${
-              options.path
-            }?customer=cus_123&subscription_items[0][plan]=foo&subscription_items[0][quantity]=2&subscription_items[1][id]=si_123&subscription_items[1][deleted]=true`,
+            `${options.path}?customer=cus_123&subscription_items[0][plan]=foo&subscription_items[0][quantity]=2&subscription_items[1][id]=si_123&subscription_items[1][deleted]=true`,
             ''
           )
           .reply(200, '{}');
