7608356864398c91ee2fc51b12ddf66b33e140c5
richardm-stripe <52928443+richardm-stripe@users.noreply.github.com>
2020-09-25 14:15:56 -0400

    More idiomatic types (#1018)
    * More idiomatic types

--- types/2020-08-27/FileLinks.d.ts
+++ types/2020-08-27/FileLinks.d.ts
@@ -89,7 +89,7 @@ declare module 'stripe' {
       /**
        * A future timestamp after which the link will no longer be usable, or `now` to expire the link immediately.
        */
-      expires_at?: 'now' | number | '';
+      expires_at?: 'now' | number | null;
 
       /**
        * Set of [key-value pairs](https://stripe.com/docs/api/metadata) that you can attach to an object. This can be useful for storing additional information about the object in a structured format. Individual keys can be unset by posting an empty value to them. All keys can be unset by posting an empty value to `metadata`.
