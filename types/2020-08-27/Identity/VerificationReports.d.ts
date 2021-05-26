56dfccb7124fc796c39d61e01ba4ab1e506eba1a
Dominic Charley-Roy <dcr@stripe.com>
2021-05-26 13:49:22 -0400

    Codegen for openapi e6f9d49

--- types/2020-08-27/Identity/VerificationReports.d.ts
+++ types/2020-08-27/Identity/VerificationReports.d.ts
@@ -282,22 +282,22 @@ declare module 'stripe' {
         namespace Options {
           interface Document {
             /**
-             * Restrict the list of allowed document type to these types.
+             * Array of strings of allowed identity document types. If the provided identity document isn't one of the allowed types, the verification check will fail with a document_type_not_allowed error code.
              */
             allowed_types?: Array<Document.AllowedType>;
 
             /**
-             * Require that the user provide an id number which will be verified.
+             * Collect an ID number and perform an [ID number check](https://stripe.com/docs/identity/verification-checks?type=id-number) with the document's extracted name and date of birth.
              */
             require_id_number?: boolean;
 
             /**
-             * Require that the user capture documents live with their webcam or phone camera.
+             * Disable image uploads, identity document images have to be captured using the device's camera.
              */
             require_live_capture?: boolean;
 
             /**
-             * Require that the user provide a selfie to compare against the document photo.
+             * Capture a face image and perform a [selfie check](https://stripe.com/docs/identity/verification-checks?type=selfie) comparing a photo ID and a picture of your user's face. [Learn more](https://stripe.com/docs/identity/selfie).
              */
             require_matching_selfie?: boolean;
           }
