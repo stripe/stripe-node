6113ad715562a50f158f835ef8df315b9ab05889
Dominic Charley-Roy <78050200+dcr-stripe@users.noreply.github.com>
2021-06-07 18:03:51 -0400

    API Updates (#1170)

--- types/2020-08-27/Terminal/Readers.d.ts
+++ types/2020-08-27/Terminal/Readers.d.ts
@@ -47,7 +47,7 @@ declare module 'stripe' {
         /**
          * The location identifier of the reader.
          */
-        location: string | null;
+        location: string | Stripe.Terminal.Location | null;
 
         /**
          * Set of [key-value pairs](https://stripe.com/docs/api/metadata) that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
