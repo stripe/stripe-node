713c73e8a77a86184867a3fd27a5a88f9eef7f0b
Dominic Charley-Roy <dcr@stripe.com>
2021-07-27 11:40:44 -0400

    Add support for fullPath on methods.

--- test/makeRequest.spec.js
+++ test/makeRequest.spec.js
@@ -3,6 +3,7 @@
 require('../testUtils');
 
 const makeRequest = require('../lib/makeRequest');
+const utils = require('../lib/utils');
 const expect = require('chai').expect;
 
 describe('makeRequest', () => {
@@ -24,6 +25,44 @@ describe('makeRequest', () => {
           stripe_account: 'bad',
         },
       ]);
+    });
+  });
+
+  describe('makeRequest with fullPath', () => {
+    it('handles urlData', async () => {
+      const args = ['hello', 'world'];
+
+      const fullPath = '/v1/parent/{param1}/child/{param2}';
+      const spec = {
+        fullPath,
+        urlParams: utils.extractUrlParams(fullPath),
+      };
+
+      let actualPath;
+      const mockSelf = {
+        // These two methods shouldn't be called when using a fullPath, as they
+        // don't rely on the resource path.
+        createResourcePathWithSymbols: () => {
+          throw new Error('Unexpected call to createResourcePathWithSymbols.');
+        },
+        createFullPath: () => {
+          throw new Error('Unexpected call to createFullPath.');
+        },
+        _request: (
+          _method,
+          _host,
+          path,
+          _body,
+          _auth,
+          _headers,
+          requestCallback
+        ) => {
+          actualPath = path;
+          requestCallback(null, 'response');
+        },
+      };
+      await makeRequest(mockSelf, args, spec, {});
+      expect(actualPath).to.equal('/v1/parent/hello/child/world');
     });
   });
 });
