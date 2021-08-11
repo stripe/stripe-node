ce38e4a56cc3c21e663c695f24e283f24266f51e
Dominic Charley-Roy <78050200+dcr-stripe@users.noreply.github.com>
2021-08-11 14:22:36 -0400

    API Updates (#1215)

--- types/2020-08-27/BillingPortal/Sessions.d.ts
+++ types/2020-08-27/BillingPortal/Sessions.d.ts
@@ -38,6 +38,11 @@ declare module 'stripe' {
         livemode: boolean;
 
         /**
+         * The IETF language tag of the locale Customer Portal is displayed in. If blank or auto, the customer's `preferred_locales` or browser's locale is used.
+         */
+        locale: Session.Locale | null;
+
+        /**
          * The account for which the session was created on behalf of. When specified, only subscriptions and invoices with this `on_behalf_of` account appear in the portal. For more information, see the [docs](https://stripe.com/docs/connect/charges-transfers#on-behalf-of). Use the [Accounts API](https://stripe.com/docs/api/accounts/object#account_object-settings-branding) to modify the `on_behalf_of` account's branding settings, which the portal displays.
          */
         on_behalf_of: string | null;
@@ -51,6 +56,57 @@ declare module 'stripe' {
          * The short-lived URL of the session that gives customers access to the customer portal.
          */
         url: string;
+      }
+
+      namespace Session {
+        type Locale =
+          | 'auto'
+          | 'bg'
+          | 'cs'
+          | 'da'
+          | 'de'
+          | 'el'
+          | 'en'
+          | 'en-AU'
+          | 'en-CA'
+          | 'en-GB'
+          | 'en-IE'
+          | 'en-IN'
+          | 'en-NZ'
+          | 'en-SG'
+          | 'es'
+          | 'es-419'
+          | 'et'
+          | 'fi'
+          | 'fil'
+          | 'fr'
+          | 'fr-CA'
+          | 'hr'
+          | 'hu'
+          | 'id'
+          | 'it'
+          | 'ja'
+          | 'ko'
+          | 'lt'
+          | 'lv'
+          | 'ms'
+          | 'mt'
+          | 'nb'
+          | 'nl'
+          | 'pl'
+          | 'pt'
+          | 'pt-BR'
+          | 'ro'
+          | 'ru'
+          | 'sk'
+          | 'sl'
+          | 'sv'
+          | 'th'
+          | 'tr'
+          | 'vi'
+          | 'zh'
+          | 'zh-HK'
+          | 'zh-TW';
       }
 
       interface SessionCreateParams {
@@ -70,6 +126,11 @@ declare module 'stripe' {
         expand?: Array<string>;
 
         /**
+         * The IETF language tag of the locale Customer Portal is displayed in. If blank or auto, the customer's `preferred_locales` or browser's locale is used.
+         */
+        locale?: SessionCreateParams.Locale;
+
+        /**
          * The `on_behalf_of` account to use for this session. When specified, only subscriptions and invoices with this `on_behalf_of` account appear in the portal. For more information, see the [docs](https://stripe.com/docs/connect/charges-transfers#on-behalf-of). Use the [Accounts API](https://stripe.com/docs/api/accounts/object#account_object-settings-branding) to modify the `on_behalf_of` account's branding settings, which the portal displays.
          */
         on_behalf_of?: string;
@@ -78,6 +139,57 @@ declare module 'stripe' {
          * The default URL to redirect customers to when they click on the portal's link to return to your website.
          */
         return_url?: string;
+      }
+
+      namespace SessionCreateParams {
+        type Locale =
+          | 'auto'
+          | 'bg'
+          | 'cs'
+          | 'da'
+          | 'de'
+          | 'el'
+          | 'en'
+          | 'en-AU'
+          | 'en-CA'
+          | 'en-GB'
+          | 'en-IE'
+          | 'en-IN'
+          | 'en-NZ'
+          | 'en-SG'
+          | 'es'
+          | 'es-419'
+          | 'et'
+          | 'fi'
+          | 'fil'
+          | 'fr'
+          | 'fr-CA'
+          | 'hr'
+          | 'hu'
+          | 'id'
+          | 'it'
+          | 'ja'
+          | 'ko'
+          | 'lt'
+          | 'lv'
+          | 'ms'
+          | 'mt'
+          | 'nb'
+          | 'nl'
+          | 'pl'
+          | 'pt'
+          | 'pt-BR'
+          | 'ro'
+          | 'ru'
+          | 'sk'
+          | 'sl'
+          | 'sv'
+          | 'th'
+          | 'tr'
+          | 'vi'
+          | 'zh'
+          | 'zh-HK'
+          | 'zh-TW';
       }
 
       class SessionsResource {
--- types/2020-08-27/Invoices.d.ts
+++ types/2020-08-27/Invoices.d.ts
@@ -83,7 +83,7 @@ declare module 'stripe' {
       /**
        * Either `charge_automatically`, or `send_invoice`. When charging automatically, Stripe will attempt to pay this invoice using the default source attached to the customer. When sending an invoice, Stripe will email this invoice to the customer with payment instructions.
        */
-      collection_method: Invoice.CollectionMethod | null;
+      collection_method: Invoice.CollectionMethod;
 
       /**
        * Time at which the object was created. Measured in seconds since the Unix epoch.
