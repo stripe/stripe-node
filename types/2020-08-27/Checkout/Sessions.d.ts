aaee2a377628327dec21f9ceeca63e6777912e98
Richard Marmorstein <richardm@stripe.com>
2021-09-01 12:27:02 -0400

    Non-optional fields

--- types/2020-08-27/Checkout/Sessions.d.ts
+++ types/2020-08-27/Checkout/Sessions.d.ts
@@ -212,14 +212,14 @@ declare module 'stripe' {
             /**
              * Enables user redeemable promotion codes on the recovered Checkout Sessions. Defaults to `false`
              */
-            allow_promotion_codes: boolean | null;
+            allow_promotion_codes: boolean;
 
             /**
              * If `true`, a recovery url will be generated to recover this Checkout Session if it
              * expires before a transaction is completed. It will be attached to the
              * Checkout Session object upon expiration.
              */
-            enabled: boolean | null;
+            enabled: boolean;
 
             /**
              * The timestamp at which the recovery URL will expire.
