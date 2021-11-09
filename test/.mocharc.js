1216f99ccaa7bc446665bc5808e9e2b73cab4e5a
Dominic Charley-Roy <dcr@stripe.com>
2021-11-09 15:01:15 -0500

    Replace unused mocha.opts with Mocha configuration file.

---
+++ test/.mocharc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Configuration for our Mocha test suite.
+module.exports = {
+  color: true,
+  // Run tests in parallel.
+  parallel: true,
+  // Recurse through all tests in the test directory.
+  recursive: true,
+};
