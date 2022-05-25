c2c86c37da5b5812e4b5c88affb8ca928871e540
Dominic Charley-Roy <dcr@stripe.com>
2022-05-25 10:22:52 -0400

    chore: Update FetchHttpClient testing to use global fetch when available instead of node-fetch.

--- test/net/FetchHttpClient.spec.js
+++ test/net/FetchHttpClient.spec.js
@@ -1,23 +1,32 @@
 'use strict';
 
 const expect = require('chai').expect;
-const fetch = require('node-fetch');
+const nodeFetchPolyfill = require('node-fetch');
 const {Readable} = require('stream');
 const {FetchHttpClient} = require('../../lib/net/FetchHttpClient');
 
+const useGlobalFetch = typeof global.fetch !== 'undefined';
+
 const createFetchHttpClient = () => {
-  return new FetchHttpClient(fetch);
+  return new FetchHttpClient(useGlobalFetch ? undefined : nodeFetchPolyfill);
 };
 
 const {createHttpClientTestSuite, ArrayReadable} = require('./helpers');
 
-describe('FetchHttpClient', () => {
+describe(`FetchHttpClient (using global fetch? ${useGlobalFetch})`, () => {
   createHttpClientTestSuite(createFetchHttpClient, (setupNock, sendRequest) => {
     describe('raw stream', () => {
       it('getRawResponse()', async () => {
         setupNock().reply(200);
         const response = await sendRequest();
-        expect(response.getRawResponse()).to.be.an.instanceOf(fetch.Response);
+
+        if (useGlobalFetch) {
+          expect(response.getRawResponse()).to.be.an.instanceOf(fetch.Response);
+        } else {
+          expect(response.getRawResponse()).to.be.an.instanceOf(
+            nodeFetchPolyfill.Response
+          );
+        }
       });
 
       it('toStream returns the body as a stream', async () => {
