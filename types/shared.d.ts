dd3c74c6d4f825443cfedb6fdb6e95a7cf86a799
Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
2022-09-15 10:16:18 -0500

    Fixing an issue with metadata's type

--- types/shared.d.ts
+++ types/shared.d.ts
@@ -4,7 +4,7 @@ declare module 'stripe' {
      * Set of key-value pairs that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
      */
     interface Metadata {
-      [name: string]: string;
+      [name: string]?: string;
     }
 
     /**
