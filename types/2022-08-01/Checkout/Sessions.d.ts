ab72f09cfbb735a78add5106a1c0b0caec73b21e
pakrym-stripe <99349468+pakrym-stripe@users.noreply.github.com>
2022-11-07 15:29:34 -0800

    API Updates (#1610)
    Codegen for openapi v206

--- types/2022-08-01/Checkout/Sessions.d.ts
+++ types/2022-08-01/Checkout/Sessions.d.ts
@@ -379,7 +379,7 @@ declare module 'stripe' {
 
           interface TaxId {
             /**
-             * The type of the tax ID, one of `eu_vat`, `br_cnpj`, `br_cpf`, `eu_oss_vat`, `gb_vat`, `nz_gst`, `au_abn`, `au_arn`, `in_gst`, `no_vat`, `za_vat`, `ch_vat`, `mx_rfc`, `sg_uen`, `ru_inn`, `ru_kpp`, `ca_bn`, `hk_br`, `es_cif`, `tw_vat`, `th_vat`, `jp_cn`, `jp_rn`, `jp_trn`, `li_uid`, `my_itn`, `us_ein`, `kr_brn`, `ca_qst`, `ca_gst_hst`, `ca_pst_bc`, `ca_pst_mb`, `ca_pst_sk`, `my_sst`, `sg_gst`, `ae_trn`, `cl_tin`, `sa_vat`, `id_npwp`, `my_frp`, `il_vat`, `ge_vat`, `ua_vat`, `is_vat`, `bg_uic`, `hu_tin`, `si_tin`, `ke_pin`, or `unknown`
+             * The type of the tax ID, one of `eu_vat`, `br_cnpj`, `br_cpf`, `eu_oss_vat`, `gb_vat`, `nz_gst`, `au_abn`, `au_arn`, `in_gst`, `no_vat`, `za_vat`, `ch_vat`, `mx_rfc`, `sg_uen`, `ru_inn`, `ru_kpp`, `ca_bn`, `hk_br`, `es_cif`, `tw_vat`, `th_vat`, `jp_cn`, `jp_rn`, `jp_trn`, `li_uid`, `my_itn`, `us_ein`, `kr_brn`, `ca_qst`, `ca_gst_hst`, `ca_pst_bc`, `ca_pst_mb`, `ca_pst_sk`, `my_sst`, `sg_gst`, `ae_trn`, `cl_tin`, `sa_vat`, `id_npwp`, `my_frp`, `il_vat`, `ge_vat`, `ua_vat`, `is_vat`, `bg_uic`, `hu_tin`, `si_tin`, `ke_pin`, `tr_tin`, `eg_tin`, `ph_tin`, or `unknown`
              */
             type: TaxId.Type;
 
@@ -405,6 +405,7 @@ declare module 'stripe' {
               | 'ca_qst'
               | 'ch_vat'
               | 'cl_tin'
+              | 'eg_tin'
               | 'es_cif'
               | 'eu_oss_vat'
               | 'eu_vat'
@@ -428,6 +429,7 @@ declare module 'stripe' {
               | 'my_sst'
               | 'no_vat'
               | 'nz_gst'
+              | 'ph_tin'
               | 'ru_inn'
               | 'ru_kpp'
               | 'sa_vat'
@@ -435,6 +437,7 @@ declare module 'stripe' {
               | 'sg_uen'
               | 'si_tin'
               | 'th_vat'
+              | 'tr_tin'
               | 'tw_vat'
               | 'ua_vat'
               | 'unknown'
