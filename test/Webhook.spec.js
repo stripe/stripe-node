530c3651b5f2d06ff1b4efbed4b01bc4352dbf1b
anniel-stripe <97691964+anniel-stripe@users.noreply.github.com>
2023-01-12 09:22:13 -0800

    Refactor usages of Node.js Buffer (#1650)
    * [wip]
    
    * Passes tests, uploads files
    
    * Clean up
    
    * instanceof Uint8Array in flattenAndStringify
    
    * Add webhook test
    
    * reviewer comments pt. 1
    
    * Add tests for Uint8Array
    
    * add type to push(l)
    
    * Reviewer comments round 2
    
    * Update WebhookHeader and WebhookPayload types

--- test/Webhook.spec.js
+++ test/Webhook.spec.js
@@ -48,6 +48,37 @@ describe('Webhooks', () => {
         expect(event.id).to.equal(EVENT_PAYLOAD.id);
       });
 
+      it('should return an Event instance from a payload and header with type Buffer', async () => {
+        const header = stripe.webhooks.generateTestHeaderString({
+          payload: EVENT_PAYLOAD_STRING,
+          secret: SECRET,
+        });
+
+        const event = await constructEventFn(
+          Buffer.from(EVENT_PAYLOAD_STRING),
+          Buffer.from(header),
+          SECRET
+        );
+
+        expect(event.id).to.equal(EVENT_PAYLOAD.id);
+      });
+
+      it('should return an Event instance from a payload and header with type Uint8Array', async () => {
+        const header = stripe.webhooks.generateTestHeaderString({
+          payload: EVENT_PAYLOAD_STRING,
+          secret: SECRET,
+        });
+
+        const textEncoder = new TextEncoder();
+        const event = await constructEventFn(
+          textEncoder.encode(EVENT_PAYLOAD_STRING),
+          textEncoder.encode(header),
+          SECRET
+        );
+
+        expect(event.id).to.equal(EVENT_PAYLOAD.id);
+      });
+
       it('should raise a JSON error from invalid JSON payload', async () => {
         const header = stripe.webhooks.generateTestHeaderString({
           payload: '} I am not valid JSON; 123][',
