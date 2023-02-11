d6540a8237b6f9b035f187722790f5691e8dbd3e
pakrym-stripe <99349468+pakrym-stripe@users.noreply.github.com>
2023-02-10 21:41:19 -0800

    Add more webhook parsing checks (#1685)

--- README.md
+++ README.md
@@ -331,7 +331,7 @@ Stripe can optionally sign the webhook events it sends to your endpoint, allowin
 
 Please note that you must pass the _raw_ request body, exactly as received from Stripe, to the `constructEvent()` function; this will not work with a parsed (i.e., JSON) request body.
 
-You can find an example of how to use this with [Express](https://expressjs.com/) in the [`examples/webhook-signing`](examples/webhook-signing) folder, but here's what it looks like:
+You can find an example of how to use this with various JavaScript frameworks in [`examples/webhook-signing`](examples/webhook-signing) folder, but here's what it looks like:
 
 ```js
 const event = stripe.webhooks.constructEvent(
--- src/Webhooks.ts
+++ src/Webhooks.ts
@@ -12,6 +12,7 @@ type WebhookParsedEvent = {
   details: WebhookParsedHeader;
   decodedPayload: WebhookHeader;
   decodedHeader: WebhookPayload;
+  suspectPayloadType: boolean;
 };
 type WebhookTestHeaderOptions = {
   timestamp: number;
@@ -163,6 +164,7 @@ function createWebhooks(platformFunctions: PlatformFunctions): WebhookObject {
         decodedHeader: header,
         decodedPayload: payload,
         details,
+        suspectPayloadType,
       } = parseEventDetails(
         encodedPayload,
         encodedHeader,
@@ -180,7 +182,8 @@ function createWebhooks(platformFunctions: PlatformFunctions): WebhookObject {
         header,
         details,
         expectedSignature,
-        tolerance
+        tolerance,
+        suspectPayloadType
       );
 
       return true;
@@ -197,6 +200,7 @@ function createWebhooks(platformFunctions: PlatformFunctions): WebhookObject {
         decodedHeader: header,
         decodedPayload: payload,
         details,
+        suspectPayloadType,
       } = parseEventDetails(
         encodedPayload,
         encodedHeader,
@@ -215,7 +219,8 @@ function createWebhooks(platformFunctions: PlatformFunctions): WebhookObject {
         header,
         details,
         expectedSignature,
-        tolerance
+        tolerance,
+        suspectPayloadType
       );
     },
   };
@@ -232,6 +237,20 @@ function createWebhooks(platformFunctions: PlatformFunctions): WebhookObject {
     encodedHeader: WebhookHeader,
     expectedScheme: string
   ): WebhookParsedEvent {
+    if (!encodedPayload) {
+      throw new StripeSignatureVerificationError(
+        encodedHeader,
+        encodedPayload,
+        {
+          message: 'No webhook payload was provided.',
+        }
+      );
+    }
+
+    const suspectPayloadType =
+      typeof encodedPayload != 'string' &&
+      !(encodedPayload instanceof Uint8Array);
+
     const textDecoder = new TextDecoder('utf8');
     const decodedPayload =
       encodedPayload instanceof Uint8Array
@@ -245,6 +264,16 @@ function createWebhooks(platformFunctions: PlatformFunctions): WebhookObject {
     if (Array.isArray(encodedHeader)) {
       throw new Error(
         'Unexpected: An array was passed as a header, which should not be possible for the stripe-signature header.'
+      );
+    }
+
+    if (encodedHeader == null || encodedHeader == '') {
+      throw new StripeSignatureVerificationError(
+        encodedHeader,
+        encodedPayload,
+        {
+          message: 'No stripe-signature header value was provided.',
+        }
       );
     }
 
@@ -279,6 +308,7 @@ function createWebhooks(platformFunctions: PlatformFunctions): WebhookObject {
       decodedPayload,
       decodedHeader,
       details,
+      suspectPayloadType,
     };
   }
 
@@ -287,19 +317,30 @@ function createWebhooks(platformFunctions: PlatformFunctions): WebhookObject {
     header: WebhookHeader,
     details: WebhookParsedHeader,
     expectedSignature: string,
-    tolerance: number
+    tolerance: number,
+    suspectPayloadType: boolean
   ): boolean {
     const signatureFound = !!details.signatures.filter(
       platformFunctions.secureCompare.bind(platformFunctions, expectedSignature)
     ).length;
 
     if (!signatureFound) {
-      // @ts-ignore
+      if (suspectPayloadType) {
+        throw new StripeSignatureVerificationError(header, payload, {
+          message:
+            'Webhook payload must be provided as a string or a Buffer (https://nodejs.org/api/buffer.html) instance representing the _raw_ request body.' +
+            'Payload was provided as a parsed JavaScript object instead. \n' +
+            'Signature verification is impossible without access to the original signed material. \n' +
+            'Learn more about webhook signing and explore webhook integration examples for various frameworks at ' +
+            'https://github.com/stripe/stripe-node#webhook-signing',
+        });
+      }
       throw new StripeSignatureVerificationError(header, payload, {
         message:
           'No signatures found matching the expected signature for payload.' +
-          ' Are you passing the raw request body you received from Stripe?' +
-          ' https://github.com/stripe/stripe-node#webhook-signing',
+          ' Are you passing the raw request body you received from Stripe? \n' +
+          'Learn more about webhook signing and explore webhook integration examples for various frameworks at ' +
+          'https://github.com/stripe/stripe-node#webhook-signing',
       });
     }
 
