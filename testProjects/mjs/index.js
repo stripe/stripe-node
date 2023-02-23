d2a0113639fc52e173d03ce5b0e2fc617b3668f4
Annie Li <anniel@stripe.com>
2023-02-23 11:38:28 -0800

    Add assertions in test projects

--- testProjects/mjs/index.js
+++ testProjects/mjs/index.js
@@ -1,8 +1,46 @@
 import {Stripe} from 'stripe';
 import DefaultStripe from 'stripe';
+import assert from 'assert';
+
+assert(Stripe.PACKAGE_VERSION);
+assert(Stripe.USER_AGENT);
+
+assert(Stripe.HttpClient);
+assert(Stripe.HttpClientResponse);
+assert(Stripe.createFetchHttpClient);
+assert(Stripe.createNodeHttpClient);
+
+assert(Stripe.CryptoProvider);
+assert(Stripe.createNodeCryptoProvider);
+assert(Stripe.createSubtleCryptoProvider);
+
+assert(Stripe.errors);
+assert(Stripe.resources);
+assert(Stripe.webhooks);
+
+assert(Stripe.StripeResource);
+assert(Stripe.StripeResource.method);
+assert(Stripe.StripeResource.extend);
+assert(Stripe.StripeResource.MAX_BUFFERED_REQUEST_METRICS);
+
+assert(Stripe.Stripe);
+assert(Stripe.default);
 
 const stripe = new Stripe(process.argv[2]);
 const defaultStripe = new DefaultStripe(process.argv[2]);
+
+assert(stripe._platformFunctions);
+assert(stripe._api);
+assert(stripe._enableTelemetry);
+assert(stripe._prevRequestMetrics);
+assert(stripe._requestSender);
+assert(stripe.VERSION);
+assert(stripe.webhookEndpoints);
+assert(stripe.webhooks);
+assert(stripe._emitter);
+assert(stripe.on);
+assert(stripe.off);
+assert(stripe.once);
 
 try {
   throw new stripe.errors.StripeError({
