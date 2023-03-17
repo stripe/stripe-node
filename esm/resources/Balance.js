03b11d1901ad56cf64d251e64f8d4b7aca91314e
anniel-stripe <97691964+anniel-stripe@users.noreply.github.com>
2023-03-17 09:30:50 -0700

    Don't check in build output (#1717)
    * Don't check in cjs and esm
    
    * Remove cjs and esm

--- esm/resources/Balance.js
+++
@@ -1,9 +0,0 @@
-// File generated from our OpenAPI spec
-import { StripeResource } from '../StripeResource.js';
-const stripeMethod = StripeResource.method;
-export const Balance = StripeResource.extend({
-    retrieve: stripeMethod({
-        method: 'GET',
-        fullPath: '/v1/balance',
-    }),
-});
--- esm/resources/Terminal/ConnectionTokens.js
+++
@@ -1,9 +0,0 @@
-// File generated from our OpenAPI spec
-import { StripeResource } from '../../StripeResource.js';
-const stripeMethod = StripeResource.method;
-export const ConnectionTokens = StripeResource.extend({
-    create: stripeMethod({
-        method: 'POST',
-        fullPath: '/v1/terminal/connection_tokens',
-    }),
-});
