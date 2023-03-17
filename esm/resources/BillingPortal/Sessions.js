25cd07f7047273482ae671a891c54c1f49269e97
Annie Li <anniel@stripe.com>
2023-03-17 08:22:17 -0700

    Remove cjs and esm

--- esm/resources/BillingPortal/Sessions.js
+++
@@ -1,9 +0,0 @@
-// File generated from our OpenAPI spec
-import { StripeResource } from '../../StripeResource.js';
-const stripeMethod = StripeResource.method;
-export const Sessions = StripeResource.extend({
-    create: stripeMethod({
-        method: 'POST',
-        fullPath: '/v1/billing_portal/sessions',
-    }),
-});
