37545d2a30ca64e03135d5473c53bda27b8c7637
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-04-21 16:12:27 +0000

    Update generated code for v308

--- types/PaymentIntentsResource.d.ts
+++ types/PaymentIntentsResource.d.ts
@@ -3777,6 +3777,11 @@ declare module 'stripe' {
       expand?: Array<string>;
 
       /**
+       * Set of [key-value pairs](https://stripe.com/docs/api/metadata) that you can attach to an object. This can be useful for storing additional information about the object in a structured format. Individual keys can be unset by posting an empty value to them. All keys can be unset by posting an empty value to `metadata`.
+       */
+      metadata?: Stripe.Emptyable<Stripe.MetadataParam>;
+
+      /**
        * For non-card charges, you can use this value as the complete description that appears on your customers' statements. Must contain at least one letter, maximum 22 characters.
        */
       statement_descriptor?: string;
