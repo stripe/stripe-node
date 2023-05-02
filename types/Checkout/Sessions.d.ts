3135128268197bf966177731dcf55b122c1cb572
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-02 22:43:36 +0000

    Update generated code for v321

--- types/Checkout/Sessions.d.ts
+++ types/Checkout/Sessions.d.ts
@@ -757,6 +757,8 @@ declare module 'stripe' {
 
           konbini?: PaymentMethodOptions.Konbini;
 
+          link?: PaymentMethodOptions.Link;
+
           oxxo?: PaymentMethodOptions.Oxxo;
 
           p24?: PaymentMethodOptions.P24;
@@ -1122,6 +1124,21 @@ declare module 'stripe' {
              * When processing card payments, Stripe also uses `setup_future_usage` to dynamically optimize your payment flow and comply with regional legislation and network rules, such as [SCA](https://stripe.com/docs/strong-customer-authentication).
              */
             setup_future_usage?: 'none';
+          }
+
+          interface Link {
+            /**
+             * Indicates that you intend to make future payments with this PaymentIntent's payment method.
+             *
+             * Providing this parameter will [attach the payment method](https://stripe.com/docs/payments/save-during-payment) to the PaymentIntent's Customer, if present, after the PaymentIntent is confirmed and any required actions from the user are complete. If no Customer was provided, the payment method can still be [attached](https://stripe.com/docs/api/payment_methods/attach) to a Customer after the transaction completes.
+             *
+             * When processing card payments, Stripe also uses `setup_future_usage` to dynamically optimize your payment flow and comply with regional legislation and network rules, such as [SCA](https://stripe.com/docs/strong-customer-authentication).
+             */
+            setup_future_usage?: Link.SetupFutureUsage;
+          }
+
+          namespace Link {
+            type SetupFutureUsage = 'none' | 'off_session';
           }
 
           interface Oxxo {
