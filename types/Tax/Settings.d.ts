26aedbc806d8911c9160dbc477c72c479ecc1c1e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-25 11:05:09 +0000

    Update generated code for v357

--- types/Tax/Settings.d.ts
+++ types/Tax/Settings.d.ts
@@ -82,7 +82,7 @@ declare module 'stripe' {
 
           interface Pending {
             /**
-             * The list of missing fields that are required to perform calculations. It includes at least one entry when the status is `pending`. It is recommended to set the optional values even if they aren't listed as required for calculating taxes. Calculations can fail if missing fields aren't explicitly provided on every call.
+             * The list of missing fields that are required to perform calculations. It includes the entry `head_office` when the status is `pending`. It is recommended to set the optional values even if they aren't listed as required for calculating taxes. Calculations can fail if missing fields aren't explicitly provided on every call.
              */
             missing_fields: Array<string> | null;
           }
