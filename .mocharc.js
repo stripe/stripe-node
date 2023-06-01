fd5178752b54035e695a8e057e03c988d55f7ef4
Richard Marmorstein <richardm@stripe.com>
2023-06-01 16:37:15 -0700

    Mocha bootstrap script

--- .mocharc.js
+++ .mocharc.js
@@ -8,12 +8,15 @@ const config = {
   // Recurse through all tests in the test directory.
   recursive: true,
   extension: ["js", "ts"],
-  require: "ts-node/register/transpile-only",
+  require: "ts-node/register/transpile-only,./test/setup",
 };
 
 if (process.argv.find(a => a.includes('WebStorm'))) {
   // Parallel doesn't work correctly when running from JetBrains WebStorm
   config.parallel = false;
 }
+
+// Ensure we are using the 'as promised' libs before any tests are run:
+require('chai').use(require('chai-as-promised'));
 
 module.exports = config;
--- test/testUtils.ts
+++ test/testUtils.ts
@@ -3,8 +3,6 @@
 // NOTE: testUtils should be require'd before anything else in each spec file!
 
 require('mocha');
-// Ensure we are using the 'as promised' libs before any tests are run:
-require('chai').use(require('chai-as-promised'));
 
 const http = require('http');
 
