637f7d7ff26302b4ad8b93b126ceb27567849839
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-06-28 22:27:26 +0000

    Update generated code for v402

--- types/Terminal/ReadersResource.d.ts
+++ types/Terminal/ReadersResource.d.ts
@@ -109,7 +109,7 @@ declare module 'stripe' {
         expand?: Array<string>;
 
         /**
-         * Metadata related to the inputs to be collected
+         * Set of [key-value pairs](https://stripe.com/docs/api/metadata) that you can attach to an object. This can be useful for storing additional information about the object in a structured format. Individual keys can be unset by posting an empty value to them. All keys can be unset by posting an empty value to `metadata`.
          */
         metadata?: Stripe.MetadataParam;
       }
