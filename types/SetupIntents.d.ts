658e1d5d6d22396e6e48ad30bae594b09caeaaf3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-07-12 21:38:54 +0000

    Update generated code for v417

--- types/SetupIntents.d.ts
+++ types/SetupIntents.d.ts
@@ -161,9 +161,20 @@ declare module 'stripe' {
     namespace SetupIntent {
       interface AutomaticPaymentMethods {
         /**
+         * Controls whether this SetupIntent will accept redirect-based payment methods.
+         *
+         * Redirect-based payment methods may require your customer to be redirected to a payment method's app or site for authentication or additional steps. To [confirm](https://stripe.com/docs/api/setup_intents/confirm) this SetupIntent, you may be required to provide a `return_url` to redirect customers back to your site after they authenticate or complete the setup.
+         */
+        allow_redirects?: AutomaticPaymentMethods.AllowRedirects;
+
+        /**
          * Automatically calculates compatible payment methods
          */
         enabled: boolean | null;
+      }
+
+      namespace AutomaticPaymentMethods {
+        type AllowRedirects = 'always' | 'never';
       }
 
       type CancellationReason =
--- types/SetupIntentsResource.d.ts
+++ types/SetupIntentsResource.d.ts
@@ -99,9 +99,20 @@ declare module 'stripe' {
     namespace SetupIntentCreateParams {
       interface AutomaticPaymentMethods {
         /**
+         * Controls whether this SetupIntent will accept redirect-based payment methods.
+         *
+         * Redirect-based payment methods may require your customer to be redirected to a payment method's app or site for authentication or additional steps. To [confirm](https://stripe.com/docs/api/setup_intents/confirm) this SetupIntent, you may be required to provide a `return_url` to redirect customers back to your site after they authenticate or complete the setup.
+         */
+        allow_redirects?: AutomaticPaymentMethods.AllowRedirects;
+
+        /**
          * Whether this feature is enabled.
          */
         enabled: boolean;
+      }
+
+      namespace AutomaticPaymentMethods {
+        type AllowRedirects = 'always' | 'never';
       }
 
       type FlowDirection = 'inbound' | 'outbound';
