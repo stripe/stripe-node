d6f691942371189dab9df97a749b38bd0ccb9931
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-07-13 19:23:25 +0000

    Update generated code for v417

--- src/apiVersion.ts
+++ src/apiVersion.ts
@@ -1,4 +1,4 @@
 // File generated from our OpenAPI spec
 
 export const ApiVersion = '2022-11-15';
-export const PreviewVersion = '2023-06-08.preview-v2';
+export const PreviewVersion = '20230712T200515';
