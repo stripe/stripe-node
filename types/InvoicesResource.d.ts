6c54d325580d0bd69bb46ef725388009272a1e08
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-02 20:12:42 +0000

    Update generated code for v441

--- types/InvoicesResource.d.ts
+++ types/InvoicesResource.d.ts
@@ -1288,12 +1288,12 @@ declare module 'stripe' {
       currency?: string;
 
       /**
-       * The identifier of the customer whose upcoming invoice you'd like to retrieve.
+       * The identifier of the customer whose upcoming invoice you'd like to retrieve. If `automatic_tax` is enabled then one of `customer`, `customer_details`, `subscription`, or `schedule` must be set.
        */
       customer?: string;
 
       /**
-       * Details about the customer you want to invoice or overrides for an existing customer.
+       * Details about the customer you want to invoice or overrides for an existing customer. If `automatic_tax` is enabled then one of `customer`, `customer_details`, `subscription`, or `schedule` must be set.
        */
       customer_details?: InvoiceListUpcomingLinesParams.CustomerDetails;
 
@@ -1873,12 +1873,12 @@ declare module 'stripe' {
       currency?: string;
 
       /**
-       * The identifier of the customer whose upcoming invoice you'd like to retrieve.
+       * The identifier of the customer whose upcoming invoice you'd like to retrieve. If `automatic_tax` is enabled then one of `customer`, `customer_details`, `subscription`, or `schedule` must be set.
        */
       customer?: string;
 
       /**
-       * Details about the customer you want to invoice or overrides for an existing customer.
+       * Details about the customer you want to invoice or overrides for an existing customer. If `automatic_tax` is enabled then one of `customer`, `customer_details`, `subscription`, or `schedule` must be set.
        */
       customer_details?: InvoiceRetrieveUpcomingParams.CustomerDetails;
 
