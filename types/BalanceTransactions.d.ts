faa60ffd2f4853ffdee0e51eface8ba05c9da6e0
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-28 16:51:02 +0000

    Update generated code for v488

--- types/BalanceTransactions.d.ts
+++ types/BalanceTransactions.d.ts
@@ -98,7 +98,7 @@ declare module 'stripe' {
       status: string;
 
       /**
-       * Transaction type: `adjustment`, `advance`, `advance_funding`, `anticipation_repayment`, `application_fee`, `application_fee_refund`, `charge`, `connect_collection_transfer`, `contribution`, `issuing_authorization_hold`, `issuing_authorization_release`, `issuing_dispute`, `issuing_transaction`, `payment`, `payment_failure_refund`, `payment_refund`, `payment_reversal`, `payout`, `payout_cancel`, `payout_failure`, `refund`, `refund_failure`, `reserve_transaction`, `reserved_funds`, `stripe_fee`, `stripe_fx_fee`, `tax_fee`, `topup`, `topup_reversal`, `transfer`, `transfer_cancel`, `transfer_failure`, or `transfer_refund`. [Learn more](https://stripe.com/docs/reports/balance-transaction-types) about balance transaction types and what they represent. If you are looking to classify transactions for accounting purposes, you might want to consider `reporting_category` instead.
+       * Transaction type: `adjustment`, `advance`, `advance_funding`, `anticipation_repayment`, `application_fee`, `application_fee_refund`, `charge`, `connect_collection_transfer`, `contribution`, `issuing_authorization_hold`, `issuing_authorization_release`, `issuing_dispute`, `issuing_transaction`, `obligation_inbound`, `obligation_outbound`, `obligation_reversal_inbound`, `obligation_reversal_outbound`, `obligation_payout`, `obligation_payout_failure`, `payment`, `payment_failure_refund`, `payment_refund`, `payment_reversal`, `payout`, `payout_cancel`, `payout_failure`, `refund`, `refund_failure`, `reserve_transaction`, `reserved_funds`, `stripe_fee`, `stripe_fx_fee`, `tax_fee`, `topup`, `topup_reversal`, `transfer`, `transfer_cancel`, `transfer_failure`, or `transfer_refund`. [Learn more](https://stripe.com/docs/reports/balance-transaction-types) about balance transaction types and what they represent. If you are looking to classify transactions for accounting purposes, you might want to consider `reporting_category` instead.
        */
       type: BalanceTransaction.Type;
     }
@@ -145,6 +145,12 @@ declare module 'stripe' {
         | 'issuing_authorization_release'
         | 'issuing_dispute'
         | 'issuing_transaction'
+        | 'obligation_inbound'
+        | 'obligation_outbound'
+        | 'obligation_payout'
+        | 'obligation_payout_failure'
+        | 'obligation_reversal_inbound'
+        | 'obligation_reversal_outbound'
         | 'payment'
         | 'payment_failure_refund'
         | 'payment_refund'
