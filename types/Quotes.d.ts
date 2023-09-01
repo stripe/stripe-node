cda1b3c4ddd0285bf33a976a75396e7afd3b5f4b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-01 19:47:53 +0000

    Update generated code for v501

--- types/Quotes.d.ts
+++ types/Quotes.d.ts
@@ -121,7 +121,7 @@ declare module 'stripe' {
       line_items?: ApiList<Stripe.LineItem>;
 
       /**
-       * A list of lines on the quote. These lines describe changes that will be used to create new subscription schedules or update existing subscription schedules when the quote is accepted.
+       * A list of lines on the quote. These lines describe changes, in the order provided, that will be used to create new subscription schedules or update existing subscription schedules when the quote is accepted.
        */
       lines?: Array<string> | null;
 
