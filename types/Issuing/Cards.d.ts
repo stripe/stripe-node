8a903c8f536ddafe452fd26876958f2b45bd8763
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-06 22:54:40 +0000

    Update generated code for v513

--- types/Issuing/Cards.d.ts
+++ types/Issuing/Cards.d.ts
@@ -243,8 +243,7 @@ declare module 'stripe' {
             | 'failure'
             | 'pending'
             | 'returned'
-            | 'shipped'
-            | 'submitted';
+            | 'shipped';
 
           type Type = 'bulk' | 'individual';
         }
