3a72f78c7be2bad35cdb601913c65bac2d5bec4d
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-12 18:18:11 +0000

    Update generated code for v523

--- types/EphemeralKeysResource.d.ts
+++ types/EphemeralKeysResource.d.ts
@@ -19,6 +19,11 @@ declare module 'stripe' {
       issuing_card?: string;
 
       /**
+       * A single-use token, created by Stripe.js, used for creating ephemeral keys for Issuing Cards without exchanging sensitive information.
+       */
+      nonce?: string;
+
+      /**
        * The ID of the Identity VerificationSession you'd like to access using the resulting ephemeral key
        */
       verification_session?: string;
