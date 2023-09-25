b987bd244fe9f9c67298fb6aabceabb30c5a1f3b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-25 12:01:13 +0000

    Update generated code for v551

--- types/PayoutsResource.d.ts
+++ types/PayoutsResource.d.ts
@@ -19,7 +19,7 @@ declare module 'stripe' {
       description?: string;
 
       /**
-       * The ID of a bank account or a card to send the payout to. If no destination is supplied, the default external account for the specified currency will be used.
+       * The ID of a bank account or a card to send the payout to. If you don't provide a destination, we use the default external account for the specified currency.
        */
       destination?: string;
 
@@ -34,17 +34,17 @@ declare module 'stripe' {
       metadata?: Stripe.MetadataParam;
 
       /**
-       * The method used to send this payout, which can be `standard` or `instant`. `instant` is supported for payouts to debit cards and bank accounts in certain countries. (See [Bank support for Instant Payouts](https://stripe.com/docs/payouts/instant-payouts-banks) for more information.)
+       * The method used to send this payout, which is `standard` or `instant`. We support `instant` for payouts to debit cards and bank accounts in certain countries. Learn more about [bank support for Instant Payouts](https://stripe.com/docs/payouts/instant-payouts-banks).
        */
       method?: PayoutCreateParams.Method;
 
       /**
-       * The balance type of your Stripe balance to draw this payout from. Balances for different payment sources are kept separately. You can find the amounts with the balances API. One of `bank_account`, `card`, or `fpx`.
+       * The balance type of your Stripe balance to draw this payout from. Balances for different payment sources are kept separately. You can find the amounts with the Balances API. One of `bank_account`, `card`, or `fpx`.
        */
       source_type?: PayoutCreateParams.SourceType;
 
       /**
-       * A string to be displayed on the recipient's bank or card statement. This may be at most 22 characters. Attempting to use a `statement_descriptor` longer than 22 characters will return an error. Note: Most banks will truncate this information and/or display it inconsistently. Some may not display it at all.
+       * A string that displays on the recipient's bank or card statement (up to 22 characters). A `statement_descriptor` that's longer than 22 characters return an error. Most banks truncate this information and display it inconsistently. Some banks might not display it at all.
        */
       statement_descriptor?: string;
     }
@@ -116,11 +116,11 @@ declare module 'stripe' {
 
     class PayoutsResource {
       /**
-       * To send funds to your own bank account, you create a new payout object. Your [Stripe balance](https://stripe.com/docs/api#balance) must be able to cover the payout amount, or you'll receive an “Insufficient Funds” error.
+       * To send funds to your own bank account, create a new payout object. Your [Stripe balance](https://stripe.com/docs/api#balance) must cover the payout amount. If it doesn't, you receive an “Insufficient Funds” error.
        *
-       * If your API key is in test mode, money won't actually be sent, though everything else will occur as if in live mode.
+       * If your API key is in test mode, money won't actually be sent, though every other action occurs as if you're in live mode.
        *
-       * If you are creating a manual payout on a Stripe account that uses multiple payment source types, you'll need to specify the source type balance that the payout should draw from. The [balance object](https://stripe.com/docs/api#balance_object) details available and pending amounts by source type.
+       * If you create a manual payout on a Stripe account that uses multiple payment source types, you need to specify the source type balance that the payout draws from. The [balance object](https://stripe.com/docs/api#balance_object) details available and pending amounts by source type.
        */
       create(
         params: PayoutCreateParams,
@@ -128,7 +128,7 @@ declare module 'stripe' {
       ): Promise<Stripe.Response<Stripe.Payout>>;
 
       /**
-       * Retrieves the details of an existing payout. Supply the unique payout ID from either a payout creation request or the payout list, and Stripe will return the corresponding payout information.
+       * Retrieves the details of an existing payout. Supply the unique payout ID from either a payout creation request or the payout list. Stripe returns the corresponding payout information.
        */
       retrieve(
         id: string,
@@ -141,7 +141,7 @@ declare module 'stripe' {
       ): Promise<Stripe.Response<Stripe.Payout>>;
 
       /**
-       * Updates the specified payout by setting the values of the parameters passed. Any parameters not provided will be left unchanged. This request accepts only the metadata as arguments.
+       * Updates the specified payout by setting the values of the parameters you pass. We don't change parameters that you don't provide. This request only accepts the metadata as arguments.
        */
       update(
         id: string,
@@ -150,7 +150,7 @@ declare module 'stripe' {
       ): Promise<Stripe.Response<Stripe.Payout>>;
 
       /**
-       * Returns a list of existing payouts sent to third-party bank accounts or that Stripe has sent you. The payouts are returned in sorted order, with the most recently created payouts appearing first.
+       * Returns a list of existing payouts sent to third-party bank accounts or payouts that Stripe sent to you. The payouts return in sorted order, with the most recently created payouts appearing first.
        */
       list(
         params?: PayoutListParams,
@@ -159,7 +159,7 @@ declare module 'stripe' {
       list(options?: RequestOptions): ApiListPromise<Stripe.Payout>;
 
       /**
-       * A previously created payout can be canceled if it has not yet been paid out. Funds will be refunded to your available balance. You may not cancel automatic Stripe payouts.
+       * You can cancel a previously created payout if it hasn't been paid out yet. Stripe refunds the funds to your available balance. You can't cancel automatic Stripe payouts.
        */
       cancel(
         id: string,
@@ -172,9 +172,9 @@ declare module 'stripe' {
       ): Promise<Stripe.Response<Stripe.Payout>>;
 
       /**
-       * Reverses a payout by debiting the destination bank account. Only payouts for connected accounts to US bank accounts may be reversed at this time. If the payout is in the pending status, /v1/payouts/:id/cancel should be used instead.
+       * Reverses a payout by debiting the destination bank account. At this time, you can only reverse payouts for connected accounts to US bank accounts. If the payout is in the pending status, use /v1/payouts/:id/cancel instead.
        *
-       * By requesting a reversal via /v1/payouts/:id/reverse, you confirm that the authorized signatory of the selected bank account has authorized the debit on the bank account and that no other authorization is required.
+       * By requesting a reversal through /v1/payouts/:id/reverse, you confirm that the authorized signatory of the selected bank account authorizes the debit on the bank account and that no other authorization is required.
        */
       reverse(
         id: string,
