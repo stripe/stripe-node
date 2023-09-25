79c7b3e346b9288c77f4d38adde64d5e6f12e601
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-25 12:11:36 +0000

    Update generated code for v552

--- types/RefundsResource.d.ts
+++ types/RefundsResource.d.ts
@@ -3,11 +3,11 @@
 declare module 'stripe' {
   namespace Stripe {
     interface RefundCreateParams {
-      /**
-       * A positive integer representing how much to refund.
-       */
       amount?: number;
 
+      /**
+       * The identifier of the charge to refund.
+       */
       charge?: string;
 
       /**
@@ -40,12 +40,26 @@ declare module 'stripe' {
        */
       origin?: 'customer_balance';
 
+      /**
+       * The identifier of the PaymentIntent to refund.
+       */
       payment_intent?: string;
 
+      /**
+       * String indicating the reason for the refund. If set, possible values are `duplicate`, `fraudulent`, and `requested_by_customer`. If you believe the charge to be fraudulent, specifying `fraudulent` as the reason will add the associated card and email to your [block lists](https://stripe.com/docs/radar/lists), and will also help us improve our fraud detection algorithms.
+       */
       reason?: RefundCreateParams.Reason;
 
+      /**
+       * Boolean indicating whether the application fee should be refunded when refunding this charge. If a full charge refund is given, the full application fee will be refunded. Otherwise, the application fee will be refunded in an amount proportional to the amount of the charge refunded. An application fee can be refunded only by the application that created the charge.
+       */
       refund_application_fee?: boolean;
 
+      /**
+       * Boolean indicating whether the transfer should be reversed when refunding this charge. The transfer will be reversed proportionally to the amount being refunded (either the entire or partial amount).
+       *
+       * A transfer can be reversed only by the application that created the charge.
+       */
       reverse_transfer?: boolean;
     }
 
@@ -100,7 +114,17 @@ declare module 'stripe' {
 
     class RefundsResource {
       /**
-       * Create a refund.
+       * When you create a new refund, you must specify a Charge or a PaymentIntent object on which to create it.
+       *
+       * Creating a new refund will refund a charge that has previously been created but not yet refunded.
+       * Funds will be refunded to the credit or debit card that was originally charged.
+       *
+       * You can optionally refund only part of a charge.
+       * You can do so multiple times, until the entire charge has been refunded.
+       *
+       * Once entirely refunded, a charge can't be refunded again.
+       * This method will raise an error when called on an already-refunded charge,
+       * or when trying to refund more money than is left on a charge.
        */
       create(
         params?: RefundCreateParams,
