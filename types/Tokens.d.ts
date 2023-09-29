abaa4e6943f9bb175eea8ae002c5552e15b68188
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-29 17:34:48 +0000

    Update generated code for v570

--- types/Tokens.d.ts
+++ types/Tokens.d.ts
@@ -6,21 +6,21 @@ declare module 'stripe' {
      * Tokenization is the process Stripe uses to collect sensitive card or bank
      * account details, or personally identifiable information (PII), directly from
      * your customers in a secure manner. A token representing this information is
-     * returned to your server to use. You should use our
+     * returned to your server to use. Use our
      * [recommended payments integrations](https://stripe.com/docs/payments) to perform this process
-     * client-side. This ensures that no sensitive card data touches your server,
+     * on the client-side. This guarantees that no sensitive card data touches your server,
      * and allows your integration to operate in a PCI-compliant way.
      *
-     * If you cannot use client-side tokenization, you can also create tokens using
-     * the API with either your publishable or secret API key. Keep in mind that if
-     * your integration uses this method, you are responsible for any PCI compliance
-     * that may be required, and you must keep your secret API key safe. Unlike with
-     * client-side tokenization, your customer's information is not sent directly to
-     * Stripe, so we cannot determine how it is handled or stored.
+     * If you can't use client-side tokenization, you can also create tokens using
+     * the API with either your publishable or secret API key. If
+     * your integration uses this method, you're responsible for any PCI compliance
+     * that it might require, and you must keep your secret API key safe. Unlike with
+     * client-side tokenization, your customer's information isn't sent directly to
+     * Stripe, so we can't determine how it's handled or stored.
      *
-     * Tokens cannot be stored or used more than once. To store card or bank account
-     * information for later use, you can create [Customer](https://stripe.com/docs/api#customers)
-     * objects or [Custom accounts](https://stripe.com/docs/api#external_accounts). Note that
+     * You can't store or use tokens more than once. To store card or bank account
+     * information for later use, create [Customer](https://stripe.com/docs/api#customers)
+     * objects or [Custom accounts](https://stripe.com/docs/api#external_accounts).
      * [Radar](https://stripe.com/docs/radar), our integrated solution for automatic fraud protection,
      * performs best with integrations that use client-side tokenization.
      */
@@ -56,7 +56,7 @@ declare module 'stripe' {
       card?: Stripe.Card;
 
       /**
-       * IP address of the client that generated the token.
+       * IP address of the client that generates the token.
        */
       client_ip: string | null;
 
@@ -76,7 +76,7 @@ declare module 'stripe' {
       type: string;
 
       /**
-       * Whether this token has already been used (tokens can be used only once).
+       * Determines if you have already used this token (you can only use tokens once).
        */
       used: boolean;
     }
