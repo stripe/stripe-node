476f39405c3c399f6b0d60df5c88139bc9f951d1
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-10-18 16:39:30 +0000

    Update generated code for v608

--- types/PaymentIntentsResource.d.ts
+++ types/PaymentIntentsResource.d.ts
@@ -1704,18 +1704,9 @@ declare module 'stripe' {
           request_extended_authorization?: boolean;
 
           /**
-           * Request ability to [increment](https://stripe.com/docs/payments/incremental-authorization) for this PaymentIntent.
-           */
-          request_incremental_authorization?: CardPresent.RequestIncrementalAuthorization;
-
-          /**
            * Request ability to [increment](https://stripe.com/docs/terminal/features/incremental-authorizations) this PaymentIntent if the combination of MCC and card brand is eligible. Check [incremental_authorization_supported](https://stripe.com/docs/api/charges/object#charge_object-payment_method_details-card_present-incremental_authorization_supported) in the [Confirm](https://stripe.com/docs/api/payment_intents/confirm) response to verify support.
            */
           request_incremental_authorization_support?: boolean;
-        }
-
-        namespace CardPresent {
-          type RequestIncrementalAuthorization = 'if_available' | 'never';
         }
 
         interface Cashapp {
@@ -4057,18 +4048,9 @@ declare module 'stripe' {
           request_extended_authorization?: boolean;
 
           /**
-           * Request ability to [increment](https://stripe.com/docs/payments/incremental-authorization) for this PaymentIntent.
-           */
-          request_incremental_authorization?: CardPresent.RequestIncrementalAuthorization;
-
-          /**
            * Request ability to [increment](https://stripe.com/docs/terminal/features/incremental-authorizations) this PaymentIntent if the combination of MCC and card brand is eligible. Check [incremental_authorization_supported](https://stripe.com/docs/api/charges/object#charge_object-payment_method_details-card_present-incremental_authorization_supported) in the [Confirm](https://stripe.com/docs/api/payment_intents/confirm) response to verify support.
            */
           request_incremental_authorization_support?: boolean;
-        }
-
-        namespace CardPresent {
-          type RequestIncrementalAuthorization = 'if_available' | 'never';
         }
 
         interface Cashapp {
@@ -6810,18 +6792,9 @@ declare module 'stripe' {
           request_extended_authorization?: boolean;
 
           /**
-           * Request ability to [increment](https://stripe.com/docs/payments/incremental-authorization) for this PaymentIntent.
-           */
-          request_incremental_authorization?: CardPresent.RequestIncrementalAuthorization;
-
-          /**
            * Request ability to [increment](https://stripe.com/docs/terminal/features/incremental-authorizations) this PaymentIntent if the combination of MCC and card brand is eligible. Check [incremental_authorization_supported](https://stripe.com/docs/api/charges/object#charge_object-payment_method_details-card_present-incremental_authorization_supported) in the [Confirm](https://stripe.com/docs/api/payment_intents/confirm) response to verify support.
            */
           request_incremental_authorization_support?: boolean;
-        }
-
-        namespace CardPresent {
-          type RequestIncrementalAuthorization = 'if_available' | 'never';
         }
 
         interface Cashapp {
