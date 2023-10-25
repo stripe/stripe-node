6ef11edc4939697a8c5abe0081bf436294d0e837
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-10-25 19:40:46 +0000

    Update generated code for v623

--- types/SetupIntents.d.ts
+++ types/SetupIntents.d.ts
@@ -302,6 +302,7 @@ declare module 'stripe' {
           | 'application_fees_not_allowed'
           | 'authentication_required'
           | 'balance_insufficient'
+          | 'balance_invalid_parameter'
           | 'bank_account_bad_routing_numbers'
           | 'bank_account_declined'
           | 'bank_account_exists'
