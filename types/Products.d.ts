f5724ae9e8919eba47d2c8a8a7fbdcf55363c6b8
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-07 19:45:29 +0000

    Update generated code for v651

--- types/Products.d.ts
+++ types/Products.d.ts
@@ -139,7 +139,7 @@ declare module 'stripe' {
         /**
          * The feature's name. Up to 80 characters long.
          */
-        name: string;
+        name?: string;
       }
 
       interface PackageDimensions {
