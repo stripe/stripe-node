fcbd095fb03a1ba281e57783b7e68e613491bd69
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-14 10:19:15 +0000

    Update generated code for v657

--- types/Checkout/Sessions.d.ts
+++ types/Checkout/Sessions.d.ts
@@ -840,6 +840,8 @@ declare module 'stripe' {
 
           paynow?: PaymentMethodOptions.Paynow;
 
+          paypal?: PaymentMethodOptions.Paypal;
+
           pix?: PaymentMethodOptions.Pix;
 
           revolut_pay?: PaymentMethodOptions.RevolutPay;
@@ -1257,6 +1259,36 @@ declare module 'stripe' {
              * When processing card payments, Stripe also uses `setup_future_usage` to dynamically optimize your payment flow and comply with regional legislation and network rules, such as [SCA](https://stripe.com/docs/strong-customer-authentication).
              */
             setup_future_usage?: 'none';
+          }
+
+          interface Paypal {
+            /**
+             * Controls when the funds will be captured from the customer's account.
+             */
+            capture_method?: 'manual';
+
+            /**
+             * Preferred locale of the PayPal checkout page that the customer is redirected to.
+             */
+            preferred_locale: string | null;
+
+            /**
+             * A reference of the PayPal transaction visible to customer which is mapped to PayPal's invoice ID. This must be a globally unique ID if you have configured in your PayPal settings to block multiple payments per invoice ID.
+             */
+            reference: string | null;
+
+            /**
+             * Indicates that you intend to make future payments with this PaymentIntent's payment method.
+             *
+             * Providing this parameter will [attach the payment method](https://stripe.com/docs/payments/save-during-payment) to the PaymentIntent's Customer, if present, after the PaymentIntent is confirmed and any required actions from the user are complete. If no Customer was provided, the payment method can still be [attached](https://stripe.com/docs/api/payment_methods/attach) to a Customer after the transaction completes.
+             *
+             * When processing card payments, Stripe also uses `setup_future_usage` to dynamically optimize your payment flow and comply with regional legislation and network rules, such as [SCA](https://stripe.com/docs/strong-customer-authentication).
+             */
+            setup_future_usage?: Paypal.SetupFutureUsage;
+          }
+
+          namespace Paypal {
+            type SetupFutureUsage = 'none' | 'off_session';
           }
 
           interface Pix {
