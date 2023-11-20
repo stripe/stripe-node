a4583715394b2569afcd84e8c7910079e0c35cb3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-20 21:39:08 +0000

    Update generated code for v678

--- types/CustomerSessionsResource.d.ts
+++ types/CustomerSessionsResource.d.ts
@@ -4,6 +4,11 @@ declare module 'stripe' {
   namespace Stripe {
     interface CustomerSessionCreateParams {
       /**
+       * Configuration for each component.
+       */
+      components: CustomerSessionCreateParams.Components;
+
+      /**
        * The ID of an existing customer for which to create the customer session.
        */
       customer: string;
@@ -12,6 +17,68 @@ declare module 'stripe' {
        * Specifies which fields in the response should be expanded.
        */
       expand?: Array<string>;
+    }
+
+    namespace CustomerSessionCreateParams {
+      interface Components {
+        /**
+         * Configuration for the payment element.
+         */
+        payment_element?: Components.PaymentElement;
+
+        /**
+         * Configuration for the pricing table.
+         */
+        pricing_table?: Components.PricingTable;
+      }
+
+      namespace Components {
+        interface PaymentElement {
+          /**
+           * Whether the payment element is enabled.
+           */
+          enabled: boolean;
+
+          /**
+           * This hash defines whether the payment element supports certain features.
+           */
+          features?: PaymentElement.Features;
+        }
+
+        namespace PaymentElement {
+          interface Features {
+            /**
+             * Whether the payment element supports detaching a payment method.
+             */
+            payment_method_detach?: Features.PaymentMethodDetach;
+
+            /**
+             * Whether the payment element supports setting a payment method as the customer's default.
+             */
+            payment_method_set_as_customer_default?: Features.PaymentMethodSetAsCustomerDefault;
+
+            /**
+             * Whether the payment element supports updating a payment method.
+             */
+            payment_method_update?: Features.PaymentMethodUpdate;
+          }
+
+          namespace Features {
+            type PaymentMethodDetach = 'auto' | 'never';
+
+            type PaymentMethodSetAsCustomerDefault = 'auto' | 'never';
+
+            type PaymentMethodUpdate = 'auto' | 'never';
+          }
+        }
+
+        interface PricingTable {
+          /**
+           * Whether pricing table is enabled.
+           */
+          enabled: boolean;
+        }
+      }
     }
 
     class CustomerSessionsResource {
