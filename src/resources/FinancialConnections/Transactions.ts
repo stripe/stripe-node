154c2f706facae229a8c7683e725c32d058af579
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-12-07 21:04:40 +0000

    Update generated code for v705

--- src/resources/FinancialConnections/Transactions.ts
+++ src/resources/FinancialConnections/Transactions.ts
@@ -3,6 +3,10 @@
 import {StripeResource} from '../../StripeResource.js';
 const stripeMethod = StripeResource.method;
 export const Transactions = StripeResource.extend({
+  retrieve: stripeMethod({
+    method: 'GET',
+    fullPath: '/v1/financial_connections/transactions/{transaction}',
+  }),
   list: stripeMethod({
     method: 'GET',
     fullPath: '/v1/financial_connections/transactions',
