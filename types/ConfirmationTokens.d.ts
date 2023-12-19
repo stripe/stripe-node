838d63a91c52fa1589dc1c43cdcf82399f20514e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-12-19 18:16:41 +0000

    Update generated code for v724

--- types/ConfirmationTokens.d.ts
+++ types/ConfirmationTokens.d.ts
@@ -6,6 +6,8 @@ declare module 'stripe' {
      * ConfirmationTokens help transport client side data collected by Stripe JS over
      * to your server for confirming a PaymentIntent or SetupIntent. If the confirmation
      * is successful, values present on the ConfirmationToken are written onto the Intent.
+     *
+     * To learn more or request access, visit the related guided: [Finalize payments on the server using Confirmation Tokens](https://stripe.com/docs/payments/finalize-payments-on-the-server-confirmation-tokens).
      */
     interface ConfirmationToken {
       /**
