c8977d4ccbdf8d5b3d95111866996419212f2b2d
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-12-20 17:50:22 +0000

    Update generated code for v729

--- types/AccountSessions.d.ts
+++ types/AccountSessions.d.ts
@@ -135,7 +135,22 @@ declare module 'stripe' {
         }
 
         namespace Payouts {
-          interface Features {}
+          interface Features {
+            /**
+             * Whether to allow payout schedule to be changed. Default `true` when Stripe owns Loss Liability, default `false` otherwise.
+             */
+            edit_payout_schedule: boolean;
+
+            /**
+             * Whether to allow creation of instant payouts. Default `true` when Stripe owns Loss Liability, default `false` otherwise.
+             */
+            instant_payouts: boolean;
+
+            /**
+             * Whether to allow creation of standard payouts. Default `true` when Stripe owns Loss Liability, default `false` otherwise.
+             */
+            standard_payouts: boolean;
+          }
         }
       }
     }
