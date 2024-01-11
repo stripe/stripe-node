aa6581a1c694e29b700837a1c661ca5c6ed7d885
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-11 17:59:57 +0000

    Update generated code for v755

--- types/CustomerSessionsResource.d.ts
+++ types/CustomerSessionsResource.d.ts
@@ -22,6 +22,11 @@ declare module 'stripe' {
     namespace CustomerSessionCreateParams {
       interface Components {
         /**
+         * Configuration for buy button.
+         */
+        buy_button?: Components.BuyButton;
+
+        /**
          * Configuration for the payment element.
          */
         payment_element?: Components.PaymentElement;
@@ -33,6 +38,13 @@ declare module 'stripe' {
       }
 
       namespace Components {
+        interface BuyButton {
+          /**
+           * Whether the buy button is enabled.
+           */
+          enabled: boolean;
+        }
+
         interface PaymentElement {
           /**
            * Whether the payment element is enabled.
