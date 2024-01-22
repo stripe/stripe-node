82528f336afb4de2a2e718ebcaec00d624856761
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-22 09:31:19 +0000

    Update generated code for v773

--- types/PayoutsResource.d.ts
+++ types/PayoutsResource.d.ts
@@ -159,7 +159,7 @@ declare module 'stripe' {
       list(options?: RequestOptions): ApiListPromise<Stripe.Payout>;
 
       /**
-       * You can cancel a previously created payout if it hasn't been paid out yet. Stripe refunds the funds to your available balance. You can't cancel automatic Stripe payouts.
+       * You can cancel a previously created payout if its status is pending. Stripe refunds the funds to your available balance. You can't cancel automatic Stripe payouts.
        */
       cancel(
         id: string,
@@ -172,7 +172,7 @@ declare module 'stripe' {
       ): Promise<Stripe.Response<Stripe.Payout>>;
 
       /**
-       * Reverses a payout by debiting the destination bank account. At this time, you can only reverse payouts for connected accounts to US bank accounts. If the payout is in the pending status, use /v1/payouts/:id/cancel instead.
+       * Reverses a payout by debiting the destination bank account. At this time, you can only reverse payouts for connected accounts to US bank accounts. If the payout is manual and in the pending status, use /v1/payouts/:id/cancel instead.
        *
        * By requesting a reversal through /v1/payouts/:id/reverse, you confirm that the authorized signatory of the selected bank account authorizes the debit on the bank account and that no other authorization is required.
        */
