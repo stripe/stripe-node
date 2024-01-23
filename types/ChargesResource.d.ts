c96c33c6fbbc30bcc772b816a5b67d7ac3265bda
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-23 18:23:12 +0000

    Update generated code for v784

--- types/ChargesResource.d.ts
+++ types/ChargesResource.d.ts
@@ -346,9 +346,9 @@ declare module 'stripe' {
 
     class ChargesResource {
       /**
-       * Use the [Payment Intents API](https://stripe.com/docs/api/payment_intents) to initiate a new payment instead
-       * of using this method. Confirmation of the PaymentIntent creates the Charge
-       * object used to request payment, so this method is limited to legacy integrations.
+       * This method is no longer recommended—use the [Payment Intents API](https://stripe.com/docs/api/payment_intents)
+       * to initiate a new payment instead. Confirmation of the PaymentIntent creates the Charge
+       * object used to request payment.
        */
       create(
         params?: ChargeCreateParams,
