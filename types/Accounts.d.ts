065200acfaf882572564fb9f5f908c4300b735fd
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-23 02:40:30 +0000

    Update generated code for v779

--- types/Accounts.d.ts
+++ types/Accounts.d.ts
@@ -647,6 +647,7 @@ declare module 'stripe' {
           | 'public_company'
           | 'public_corporation'
           | 'public_partnership'
+          | 'registered_charity'
           | 'single_member_llc'
           | 'sole_establishment'
           | 'sole_proprietorship'
