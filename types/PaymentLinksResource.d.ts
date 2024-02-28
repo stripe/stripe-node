2b65c54f8f36090bff57a06bc42cb741552c5c32
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-02-28 09:52:54 +0000

    Update generated code for v852

--- types/PaymentLinksResource.d.ts
+++ types/PaymentLinksResource.d.ts
@@ -34,7 +34,7 @@ declare module 'stripe' {
       automatic_tax?: PaymentLinkCreateParams.AutomaticTax;
 
       /**
-       * Configuration for collecting the customer's billing address.
+       * Configuration for collecting the customer's billing address. Defaults to `auto`.
        */
       billing_address_collection?: PaymentLinkCreateParams.BillingAddressCollection;
 
@@ -96,7 +96,7 @@ declare module 'stripe' {
       /**
        * Specify whether Checkout should collect a payment method. When set to `if_required`, Checkout will not collect a payment method when the total due for the session is 0.This may occur if the Checkout Session includes a free trial or a discount.
        *
-       * Can only be set in `subscription` mode.
+       * Can only be set in `subscription` mode. Defaults to `always`.
        *
        * If you'd like information on how to collect a payment method outside of Checkout, read the guide on [configuring subscriptions with a free trial](https://stripe.com/docs/payments/checkout/free-trials).
        */
@@ -1033,7 +1033,7 @@ declare module 'stripe' {
       automatic_tax?: PaymentLinkUpdateParams.AutomaticTax;
 
       /**
-       * Configuration for collecting the customer's billing address.
+       * Configuration for collecting the customer's billing address. Defaults to `auto`.
        */
       billing_address_collection?: PaymentLinkUpdateParams.BillingAddressCollection;
 
@@ -1087,7 +1087,7 @@ declare module 'stripe' {
       /**
        * Specify whether Checkout should collect a payment method. When set to `if_required`, Checkout will not collect a payment method when the total due for the session is 0.This may occur if the Checkout Session includes a free trial or a discount.
        *
-       * Can only be set in `subscription` mode.
+       * Can only be set in `subscription` mode. Defaults to `always`.
        *
        * If you'd like information on how to collect a payment method outside of Checkout, read the guide on [configuring subscriptions with a free trial](https://stripe.com/docs/payments/checkout/free-trials).
        */
