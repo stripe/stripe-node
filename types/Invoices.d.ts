545d8aa708a092bdc2b00143e6a6e90d0e78e2b3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-06 22:54:22 +0000

    Update generated code for v866

--- types/Invoices.d.ts
+++ types/Invoices.d.ts
@@ -1307,7 +1307,8 @@ declare module 'stripe' {
 
       interface SubscriptionDetails {
         /**
-         * Set of [key-value pairs](https://stripe.com/docs/api/metadata) that will reflect the metadata of the subscription at the time of invoice creation. *Note: This attribute is populated only for invoices created on or after June 29, 2023.*
+         * Set of [key-value pairs](https://stripe.com/docs/api/metadata) defined as subscription metadata when an invoice is created. Becomes an immutable snapshot of the subscription metadata at the time of invoice finalization.
+         *  *Note: This attribute is populated only for invoices created on or after June 29, 2023.*
          */
         metadata: Stripe.Metadata | null;
       }
