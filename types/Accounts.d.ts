2989412c0544648ff240d60280bd0ce7aadd00bf
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-03 23:46:09 +0000

    Update generated code for v930

--- types/Accounts.d.ts
+++ types/Accounts.d.ts
@@ -173,7 +173,7 @@ declare module 'stripe' {
       namespace BusinessProfile {
         interface AnnualRevenue {
           /**
-           * A non-negative integer representing the amount in the [smallest currency unit](https://stripe.com/docs/currencies#zero-decimal).
+           * A non-negative integer representing the amount in the [smallest currency unit](https://docs.stripe.com/currencies#zero-decimal).
            */
           amount: number | null;
 
@@ -190,7 +190,7 @@ declare module 'stripe' {
 
         interface MonthlyEstimatedRevenue {
           /**
-           * A non-negative integer representing how much to charge in the [smallest currency unit](https://stripe.com/docs/currencies#zero-decimal).
+           * A non-negative integer representing how much to charge in the [smallest currency unit](https://docs.stripe.com/currencies#zero-decimal).
            */
           amount: number;
 
