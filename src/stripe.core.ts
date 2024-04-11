dc5e9444a1f0966a48b67613cbc50688026a60f1
Ramya Rao <ramya@stripe.com>
2024-04-11 15:59:26 -0700

    Bump version to 15.1.0

--- src/stripe.core.ts
+++ src/stripe.core.ts
@@ -49,7 +49,7 @@ export function createStripe(
   platformFunctions: PlatformFunctions,
   requestSender: RequestSenderFactory = defaultRequestSenderFactory
 ): typeof Stripe {
-  Stripe.PACKAGE_VERSION = '15.0.0';
+  Stripe.PACKAGE_VERSION = '15.1.0';
   Stripe.USER_AGENT = {
     bindings_version: Stripe.PACKAGE_VERSION,
     lang: 'node',
