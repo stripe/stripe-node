1a000b577a49373d95c231f25bc1a937908aeef0
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-17 18:01:57 +0000

    Update generated code for v964

--- types/Billing/MeterEventsResource.d.ts
+++ types/Billing/MeterEventsResource.d.ts
@@ -10,7 +10,7 @@ declare module 'stripe' {
         event_name: string;
 
         /**
-         * The payload of the event. This must contain a field with the event's numerical value and a field to map the event to a customer.
+         * The payload of the event. This must contain the fields corresponding to a meter's `customer_mapping.event_payload_key` (default is `stripe_customer_id`) and `value_settings.event_payload_key` (default is `value`). Read more about the [payload](https://docs.stripe.com/billing/subscriptions/usage-based/recording-usage#payload-key-overrides).
          */
         payload: {
           [key: string]: string;
