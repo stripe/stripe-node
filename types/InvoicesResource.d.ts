2daf407fcd4c46b8dbcc863bb8c38d1236a12abc
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-24 22:00:45 +0000

    Update generated code for v989

--- types/InvoicesResource.d.ts
+++ types/InvoicesResource.d.ts
@@ -9730,7 +9730,7 @@ declare module 'stripe' {
        *
        * Note that when you are viewing an upcoming invoice, you are simply viewing a preview – the invoice has not yet been created. As such, the upcoming invoice will not show up in invoice listing calls, and you cannot use the API to pay or edit the invoice. If you want to change the amount that your customer will be billed, you can add, remove, or update pending invoice items, or update the customer's discount.
        *
-       * You can preview the effects of updating a subscription, including a preview of what proration will take place. To ensure that the actual proration is calculated exactly the same as the previewed proration, you should pass the subscription_proration_date parameter when doing the actual subscription update. The recommended way to get only the prorations being previewed is to consider only proration line items where period[start] is equal to the subscription_proration_date value passed in the request.
+       * You can preview the effects of updating a subscription, including a preview of what proration will take place. To ensure that the actual proration is calculated exactly the same as the previewed proration, you should pass the subscription_details.proration_date parameter when doing the actual subscription update. The recommended way to get only the prorations being previewed is to consider only proration line items where period[start] is equal to the subscription_details.proration_date value passed in the request.
        */
       retrieveUpcoming(
         params?: InvoiceRetrieveUpcomingParams,
