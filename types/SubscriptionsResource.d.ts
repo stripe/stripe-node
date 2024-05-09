2ad866aa356ff62fbef062b069833f9373313936
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-09 19:15:04 +0000

    Update generated code for v1020

--- types/SubscriptionsResource.d.ts
+++ types/SubscriptionsResource.d.ts
@@ -158,11 +158,6 @@ declare module 'stripe' {
       >;
 
       /**
-       * How to handle a customer's pending invoice items if an invoice will be generated. Defaults to `include` if the parameter is omitted.
-       */
-      pending_invoice_items_behavior?: SubscriptionCreateParams.PendingInvoiceItemsBehavior;
-
-      /**
        * The ID of a promotion code to apply to this subscription. A promotion code applied to a subscription will only affect invoices created for that particular subscription. This field has been deprecated and will be removed in a future API version. Use `discounts` instead.
        */
       promotion_code?: string;
@@ -801,8 +796,6 @@ declare module 'stripe' {
       namespace PendingInvoiceItemInterval {
         type Interval = 'day' | 'month' | 'week' | 'year';
       }
-
-      type PendingInvoiceItemsBehavior = 'exclude' | 'include';
 
       type ProrationBehavior = 'always_invoice' | 'create_prorations' | 'none';
 
