de5832a0a3030f61949c718454ee6336bcc3c62a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-10 01:04:48 +0000

    Update generated code for v1024

--- types/CustomerSessionsResource.d.ts
+++ types/CustomerSessionsResource.d.ts
@@ -60,12 +60,16 @@ declare module 'stripe' {
         namespace PaymentElement {
           interface Features {
             /**
-             * Controls whether the Payment Element displays the option to remove a saved payment method.
+             * Controls whether the Payment Element displays the option to remove a saved payment method."
+             *
+             * Allowing buyers to remove their saved payment methods impacts subscriptions that depend on that payment method. Removing the payment method detaches the [`customer` object](https://docs.stripe.com/api/payment_methods/object#payment_method_object-customer) from that [PaymentMethod](https://docs.stripe.com/api/payment_methods).
              */
             payment_method_remove?: Features.PaymentMethodRemove;
 
             /**
              * Controls whether the Payment Element displays a checkbox offering to save a new payment method.
+             *
+             * If a customer checks the box, the [`allow_redisplay`](https://docs.stripe.com/api/payment_methods/object#payment_method_object-allow_redisplay) value on the PaymentMethod is set to `'always'` at confirmation time. For PaymentIntents, the [`setup_future_usage`](https://docs.stripe.com/api/payment_intents/object#payment_intent_object-setup_future_usage) value is also set to the value defined in `payment_method_save_usage`.
              */
             payment_method_save?: Features.PaymentMethodSave;
 
