1e848696915269cea29cad08ab840f44e4082e7a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-14 21:14:29 +0000

    Update generated code for v1035

--- types/ProductsResource.d.ts
+++ types/ProductsResource.d.ts
@@ -62,7 +62,7 @@ declare module 'stripe' {
        * An arbitrary string to be displayed on your customer's credit card or bank statement. While most banks display this information consistently, some may display it incorrectly or not at all.
        *
        * This may be up to 22 characters. The statement description may not include `<`, `>`, `\`, `"`, `'` characters, and will appear on your customer's statement in capital letters. Non-ASCII characters are automatically stripped.
-       *  It must contain at least one letter.
+       *  It must contain at least one letter. Only used for subscription payments.
        */
       statement_descriptor?: string;
 
@@ -320,7 +320,7 @@ declare module 'stripe' {
        * An arbitrary string to be displayed on your customer's credit card or bank statement. While most banks display this information consistently, some may display it incorrectly or not at all.
        *
        * This may be up to 22 characters. The statement description may not include `<`, `>`, `\`, `"`, `'` characters, and will appear on your customer's statement in capital letters. Non-ASCII characters are automatically stripped.
-       *  It must contain at least one letter. May only be set if `type=service`.
+       *  It must contain at least one letter. May only be set if `type=service`. Only used for subscription payments.
        */
       statement_descriptor?: string;
 
