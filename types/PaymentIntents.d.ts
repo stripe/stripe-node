4a4d64ba2283334cf8d216c013816cf770d69365
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-15 22:15:17 +0000

    Update generated code for v1038

--- types/PaymentIntents.d.ts
+++ types/PaymentIntents.d.ts
@@ -1757,6 +1757,21 @@ declare module 'stripe' {
            * Request ability to [increment](https://stripe.com/docs/terminal/features/incremental-authorizations) this PaymentIntent if the combination of MCC and card brand is eligible. Check [incremental_authorization_supported](https://stripe.com/docs/api/charges/object#charge_object-payment_method_details-card_present-incremental_authorization_supported) in the [Confirm](https://stripe.com/docs/api/payment_intents/confirm) response to verify support.
            */
           request_incremental_authorization_support: boolean | null;
+
+          routing?: CardPresent.Routing;
+        }
+
+        namespace CardPresent {
+          interface Routing {
+            /**
+             * Requested routing priority
+             */
+            requested_priority: Routing.RequestedPriority | null;
+          }
+
+          namespace Routing {
+            type RequestedPriority = 'domestic' | 'international';
+          }
         }
 
         interface Cashapp {
