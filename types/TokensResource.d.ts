962cd230ebc088b9d33ec6fe9b9c8d0588559812
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-15 01:35:22 +0000

    Update generated code for v1037

--- types/TokensResource.d.ts
+++ types/TokensResource.d.ts
@@ -482,7 +482,7 @@ declare module 'stripe' {
         payment_method?: string;
 
         /**
-         * The routing number, sort code, or other country-appropriateinstitution number for the bank account. For US bank accounts, this is required and should bethe ACH routing number, not the wire routing number. If you are providing an IBAN for`account_number`, this field is not required.
+         * The routing number, sort code, or other country-appropriate institution number for the bank account. For US bank accounts, this is required and should be the ACH routing number, not the wire routing number. If you are providing an IBAN for `account_number`, this field is not required.
          */
         routing_number?: string;
       }
