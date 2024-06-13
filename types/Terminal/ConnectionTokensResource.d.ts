5c71e964b87c0b1906963931a892d7730b535054
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-06-13 22:15:34 +0000

    Update generated code for v1078

--- types/Terminal/ConnectionTokensResource.d.ts
+++ types/Terminal/ConnectionTokensResource.d.ts
@@ -10,7 +10,7 @@ declare module 'stripe' {
         expand?: Array<string>;
 
         /**
-         * The id of the location that this connection token is scoped to. If specified the connection token will only be usable with readers assigned to that location, otherwise the connection token will be usable with all readers. Note that location scoping only applies to internet-connected readers. For more details, see [the docs on scoping connection tokens](https://stripe.com/docs/terminal/fleet/locations#connection-tokens).
+         * The id of the location that this connection token is scoped to. If specified the connection token will only be usable with readers assigned to that location, otherwise the connection token will be usable with all readers. Note that location scoping only applies to internet-connected readers. For more details, see [the docs on scoping connection tokens](https://docs.stripe.com/terminal/fleet/locations-and-zones?dashboard-or-api=api#connection-tokens).
          */
         location?: string;
       }
