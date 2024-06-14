fb2decd00923898456caa31fc49462715339c264
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-06-14 20:44:26 +0000

    Update generated code for v1082

--- types/AccountsResource.d.ts
+++ types/AccountsResource.d.ts
@@ -3661,7 +3661,7 @@ declare module 'stripe' {
       /**
        * To request a new capability for an account, pass true. There can be a delay before the requested capability becomes active. If the capability has any activation requirements, the response includes them in the `requirements` arrays.
        *
-       * If a capability isn't permanent, you can remove it from the account by passing false. Most capabilities are permanent after they've been requested. Attempting to remove a permanent capability returns an error.
+       * If a capability isn't permanent, you can remove it from the account by passing false. Some capabilities are permanent after they've been requested. Attempting to remove a permanent capability returns an error.
        */
       requested?: boolean;
     }
