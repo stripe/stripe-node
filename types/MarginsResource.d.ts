06deb237aab714583bcfe76caa0714e381b1d6b7
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-06-27 18:04:22 +0000

    Update generated code for v1104

--- types/MarginsResource.d.ts
+++ types/MarginsResource.d.ts
@@ -60,7 +60,7 @@ declare module 'stripe' {
 
     interface MarginListParams extends PaginationParams {
       /**
-       * Only return margins that are active or inactive, i.e., pass false to list all inactive margins.
+       * Only return margins that are active or inactive. For example, pass `true` to only list active margins.
        */
       active?: boolean;
 
