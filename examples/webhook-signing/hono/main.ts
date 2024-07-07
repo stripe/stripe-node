ef7776d651b3b44d3b8eb6e996685ed6e9e156ee
Colin Ogoo <ogoo.colin@gmail.com>
2024-07-07 20:19:40 +0000

    feat(example/hono): example for hono
    Add stripe webhook signing example for Hono JS

---
+++ examples/webhook-signing/hono/main.ts
@@ -0,0 +1,61 @@
+#!/usr/bin/env -S npm run-script run
+
+import {serve} from '@hono/node-server';
+import {Hono} from 'hono';
+import Stripe from 'stripe';
+import env from 'dotenv';
+
+env.config();
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+const app = new Hono();
+
+app.post('/webhook', async (context) => {
+  const header = context.req.header('stripe-signature');
+
+  try {
+    const payload = await context.req.text();
+    const event = await stripe.webhooks.constructEventAsync(
+      payload,
+      header,
+      webhookSecret
+    );
+
+    console.log('Success condtructing event:', event.id);
+
+    switch (event.type) {
+      case 'payment_intent.created': {
+        console.log('PaymentIntent', event.data.object);
+        break;
+      }
+      case 'charge.succeeded': {
+        console.log('Charge id:', event.data.object.id);
+        break;
+      }
+
+      default:
+        console.log('Unhandled event type:', event.type);
+        break;
+    }
+
+    return context.json({received: true}, 200);
+  } catch (err) {
+    const errorMessage = `⚠️  Webhook signature verification failed. ${
+      err instanceof Error ? err.message : 'Internal server error'
+    }`;
+    console.log(errorMessage);
+    return context.text(errorMessage, 400);
+  }
+});
+
+export default app;
+
+const port = 8741;
+console.log(`Webhook endpoint available at http://localhost:${port}/webhook`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
