1be28b7b92f6d8fdf1a01b8bd96f60a09db3c6c4
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-08 17:57:07 +0000

    Update generated code for v1188

--- types/ConfirmationTokens.d.ts
+++ types/ConfirmationTokens.d.ts
@@ -606,6 +606,11 @@ declare module 'stripe' {
                    * Time at which the payment was collected while offline
                    */
                   stored_at: number | null;
+
+                  /**
+                   * The method used to process this payment method offline. Only deferred is allowed.
+                   */
+                  type: 'deferred' | null;
                 }
 
                 type ReadMethod =
@@ -886,6 +891,11 @@ declare module 'stripe' {
              * Time at which the payment was collected while offline
              */
             stored_at: number | null;
+
+            /**
+             * The method used to process this payment method offline. Only deferred is allowed.
+             */
+            type: 'deferred' | null;
           }
 
           type ReadMethod =
--- types/PaymentMethods.d.ts
+++ types/PaymentMethods.d.ts
@@ -508,6 +508,11 @@ declare module 'stripe' {
                  * Time at which the payment was collected while offline
                  */
                 stored_at: number | null;
+
+                /**
+                 * The method used to process this payment method offline. Only deferred is allowed.
+                 */
+                type: 'deferred' | null;
               }
 
               type ReadMethod =
@@ -788,6 +793,11 @@ declare module 'stripe' {
            * Time at which the payment was collected while offline
            */
           stored_at: number | null;
+
+          /**
+           * The method used to process this payment method offline. Only deferred is allowed.
+           */
+          type: 'deferred' | null;
         }
 
         type ReadMethod =
