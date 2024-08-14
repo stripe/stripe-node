1304e3d18052350d85e4014e8c3b0375d064fd20
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-14 16:54:46 +0000

    Update generated code for v1199

--- types/SetupIntents.d.ts
+++ types/SetupIntents.d.ts
@@ -125,7 +125,7 @@ declare module 'stripe' {
       on_behalf_of: string | Stripe.Account | null;
 
       /**
-       * ID of the payment method used with this SetupIntent. If the payment method is `card_present` and isn't a digital wallet, then the [generated_card](https://docs.corp.stripe.com/api/setup_attempts/object#setup_attempt_object-payment_method_details-card_present-generated_card) associated with the `latest_attempt` is attached to the Customer instead.
+       * ID of the payment method used with this SetupIntent. If the payment method is `card_present` and isn't a digital wallet, then the [generated_card](https://docs.stripe.com/api/setup_attempts/object#setup_attempt_object-payment_method_details-card_present-generated_card) associated with the `latest_attempt` is attached to the Customer instead.
        */
       payment_method: string | Stripe.PaymentMethod | null;
 
