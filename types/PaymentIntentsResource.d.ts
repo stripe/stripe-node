605fc203187130b504c35dbd247bb47adf950dc1
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-22 00:41:24 +0000

    Update generated code for v1212

--- types/PaymentIntentsResource.d.ts
+++ types/PaymentIntentsResource.d.ts
@@ -10958,9 +10958,7 @@ declare module 'stripe' {
       metadata?: Stripe.MetadataParam;
 
       /**
-       * Text that appears on the customer's statement as the statement descriptor for a non-card charge. This value overrides the account's default statement descriptor. For information about requirements, including the 22-character limit, see [the Statement Descriptor docs](https://docs.stripe.com/get-started/account/statement-descriptors).
-       *
-       * Setting this value for a card charge returns an error. For card charges, set the [statement_descriptor_suffix](https://docs.stripe.com/get-started/account/statement-descriptors#dynamic) instead.
+       * Text that appears on the customer's statement as the statement descriptor for a non-card or card charge. This value overrides the account's default statement descriptor. For information about requirements, including the 22-character limit, see [the Statement Descriptor docs](https://docs.stripe.com/get-started/account/statement-descriptors).
        */
       statement_descriptor?: string;
 
