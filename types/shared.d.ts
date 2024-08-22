1a428e2bb04e7150583f780c8476408e20ce5836
James Jansson <jamesjansson@gmail.com>
2024-08-23 00:17:41 +1000

    Update shared.d.ts

--- types/shared.d.ts
+++ types/shared.d.ts
@@ -3,9 +3,7 @@ declare module 'stripe' {
     /**
      * Set of key-value pairs that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
      */
-    interface Metadata {
-      [name: string]: string | undefined;
-    }
+    type Metadata = Partial<Record<string, string>>;
 
     /**
      * Set of key-value pairs that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
