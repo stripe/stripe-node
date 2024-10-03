5625aae2ab10796690903a5ad8f46d76d26ccd00
Jesse Rosalia <jar@stripe.com>
2024-10-03 15:57:33 -0700

    WIP add fetchRelatedObject, using a StripeResource to apply stripeMethod to make the retrieve call

--- src/resources/V2/Core/Events.ts
+++ src/resources/V2/Core/Events.ts
@@ -1,12 +1,52 @@
-// File generated from our OpenAPI spec
-
 import {StripeResource} from '../../../StripeResource.js';
+import {StripeResourceObject} from '../../../Types.js';
 const stripeMethod = StripeResource.method;
+const RelatedObjectResource = StripeResource.extend({});
+
 export const Events = StripeResource.extend({
-  retrieve: stripeMethod({method: 'GET', fullPath: '/v2/core/events/{id}'}),
+  constructor: function(...args: any) {
+    StripeResource.apply(this, args);
+    this.relatedResource = new RelatedObjectResource(...args);
+  },
+  // retrieve: stripeMethod({method: 'GET', fullPath: '/v2/core/events/{id}'}),
+  retrieve(...args: any[]) {
+    return stripeMethod({
+      method: 'GET',
+      fullPath: '/v2/core/events/{id}',
+    })
+      .apply(this, args)
+      .then((pulledEvent) => {
+        return addFetchObjectToPulledEvent(this.relatedResource, pulledEvent);
+      });
+  },
   list: stripeMethod({
     method: 'GET',
     fullPath: '/v2/core/events',
     methodType: 'list',
   }),
 });
+
+const addFetchObjectToPulledEvent = (
+  resource: StripeResourceObject,
+  pulledEvent: {
+    related_object: any;
+    context?: string;
+  }
+): any => {
+  if (!pulledEvent.related_object || !pulledEvent.related_object.url) {
+    return pulledEvent;
+  }
+  return {
+    ...pulledEvent,
+    fetchRelatedObject: (): Promise<null | any> => {
+      return stripeMethod({
+        method: 'GET',
+        fullPath: pulledEvent.related_object.url,
+      }).apply(resource, [
+        {
+          stripeAccount: pulledEvent.context,
+        },
+      ]);
+    },
+  };
+};
