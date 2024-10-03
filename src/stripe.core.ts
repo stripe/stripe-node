085c44492fe22a4ff9cd2c94941617d83d0e9b0a
Ramya Rao <ramya@stripe.com>
2024-10-03 16:23:18 -0700

    Bump version to 17.2.0-beta.1

--- src/stripe.core.ts
+++ src/stripe.core.ts
@@ -59,7 +59,7 @@ export function createStripe(
   platformFunctions: PlatformFunctions,
   requestSender: RequestSenderFactory = defaultRequestSenderFactory
 ): typeof Stripe {
-  Stripe.PACKAGE_VERSION = '16.13.0-beta.1';
+  Stripe.PACKAGE_VERSION = '17.2.0-beta.1';
   Stripe.USER_AGENT = {
     bindings_version: Stripe.PACKAGE_VERSION,
     lang: 'node',
