c84688980f2beb09575ae53b6a6df671d3857245
Jesse Rosalia <jar@stripe.com>
2024-10-03 17:38:42 -0700

    renamed decoratePulledEvent to addFetchRelatedObjectIfNeeded moved calls to addFetchRelatedObjectIfNeeded to transformResponseData functions; this should let this work with autoPagination as well

--- src/resources/V2/Core/Events.ts
+++ src/resources/V2/Core/Events.ts
@@ -21,27 +21,32 @@ export const Events = StripeResource.extend({
     this.relatedResource = new RelatedObjectResource(...args);
   },
   retrieve(...args: any[]) {
+    const transformResponseData = (response: any): any => {
+      return this.addFetchRelatedObjectIfNeeded(response);
+    };
     return stripeMethod({
       method: 'GET',
       fullPath: '/v2/core/events/{id}',
-    })
-      .apply(this, args)
-      .then(this.decoratePulledEvent.bind(this));
+      transformResponseData,
+    }).apply(this, args);
   },
 
   list(...args: any[]) {
+    const transformResponseData = (response: any): any => {
+      return {
+        ...response,
+        data: response.data.map(this.addFetchRelatedObjectIfNeeded.bind(this)),
+      };
+    };
     return stripeMethod({
       method: 'GET',
       fullPath: '/v2/core/events',
       methodType: 'list',
-    })
-      .apply(this, args)
-      .then((pulledEvents) => {
-        return pulledEvents.map(this.decoratePulledEvent.bind(this));
-      });
+      transformResponseData,
+    }).apply(this, args);
   },
 
-  decoratePulledEvent(pulledEvent: any) {
+  addFetchRelatedObjectIfNeeded(pulledEvent: any) {
     if (!pulledEvent.related_object || !pulledEvent.related_object.url) {
       return pulledEvent;
     }
