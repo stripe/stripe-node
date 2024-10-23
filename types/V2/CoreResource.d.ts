c41d98a707bd9274defc89e37f47038e683ee203
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-10-23 23:17:15 +0000

    Update generated code for v1318

--- types/V2/CoreResource.d.ts
+++ types/V2/CoreResource.d.ts
@@ -4,6 +4,7 @@ declare module 'stripe' {
   namespace Stripe {
     namespace V2 {
       class CoreResource {
+        eventDestinations: Stripe.V2.Core.EventDestinationsResource;
         events: Stripe.V2.Core.EventsResource;
       }
     }
