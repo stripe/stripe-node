2f7a544c0d8e63194b601cf271b229b7f6a457ee
prathmesh-stripe <165320323+prathmesh-stripe@users.noreply.github.com>
2024-10-30 18:34:11 -0400

    Fixed API version in types (#2218)

--- types/test/typescriptTest.ts
+++ types/test/typescriptTest.ts
@@ -9,7 +9,7 @@
 import Stripe from 'stripe';
 
 let stripe = new Stripe('sk_test_123', {
-  apiVersion: '2024-09-30.acacia',
+  apiVersion: '2024-10-28.acacia',
 });
 
 stripe = new Stripe('sk_test_123');
@@ -26,7 +26,7 @@ stripe = new Stripe('sk_test_123', {
 
 // Check config object.
 stripe = new Stripe('sk_test_123', {
-  apiVersion: '2024-09-30.acacia',
+  apiVersion: '2024-10-28.acacia',
   typescript: true,
   maxNetworkRetries: 1,
   timeout: 1000,
@@ -44,7 +44,7 @@ stripe = new Stripe('sk_test_123', {
     description: 'test',
   };
   const opts: Stripe.RequestOptions = {
-    apiVersion: '2024-09-30.acacia',
+    apiVersion: '2024-10-28.acacia',
   };
   const customer: Stripe.Customer = await stripe.customers.create(params, opts);
 
