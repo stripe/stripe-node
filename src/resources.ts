98f1ae96d5be3443c6f3129bc9612f660a5a6ca9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-11-04 20:08:38 +0000

    Update generated code for v1328

--- src/resources.ts
+++ src/resources.ts
@@ -26,6 +26,8 @@ import {DebitReversals as TreasuryDebitReversals} from './resources/Treasury/Deb
 import {DisputeSettlementDetails as IssuingDisputeSettlementDetails} from './resources/Issuing/DisputeSettlementDetails.js';
 import {Disputes as IssuingDisputes} from './resources/Issuing/Disputes.js';
 import {EarlyFraudWarnings as RadarEarlyFraudWarnings} from './resources/Radar/EarlyFraudWarnings.js';
+import {EventDestinations as V2CoreEventDestinations} from './resources/V2/Core/EventDestinations.js';
+import {Events as V2CoreEvents} from './resources/V2/Core/Events.js';
 import {Features as EntitlementsFeatures} from './resources/Entitlements/Features.js';
 import {FinancialAccounts as TreasuryFinancialAccounts} from './resources/Treasury/FinancialAccounts.js';
 import {FinancingOffers as CapitalFinancingOffers} from './resources/Capital/FinancingOffers.js';
@@ -38,7 +40,11 @@ import {InboundTransfers as TreasuryInboundTransfers} from './resources/Treasury
 import {Institutions as FinancialConnectionsInstitutions} from './resources/FinancialConnections/Institutions.js';
 import {Locations as TerminalLocations} from './resources/Terminal/Locations.js';
 import {MeterEventAdjustments as BillingMeterEventAdjustments} from './resources/Billing/MeterEventAdjustments.js';
+import {MeterEventAdjustments as V2BillingMeterEventAdjustments} from './resources/V2/Billing/MeterEventAdjustments.js';
+import {MeterEventSession as V2BillingMeterEventSession} from './resources/V2/Billing/MeterEventSession.js';
+import {MeterEventStream as V2BillingMeterEventStream} from './resources/V2/Billing/MeterEventStream.js';
 import {MeterEvents as BillingMeterEvents} from './resources/Billing/MeterEvents.js';
+import {MeterEvents as V2BillingMeterEvents} from './resources/V2/Billing/MeterEvents.js';
 import {Meters as BillingMeters} from './resources/Billing/Meters.js';
 import {Orders as ClimateOrders} from './resources/Climate/Orders.js';
 import {OutboundPayments as TestHelpersTreasuryOutboundPayments} from './resources/TestHelpers/Treasury/OutboundPayments.js';
@@ -136,7 +142,6 @@ export {TaxRates} from './resources/TaxRates.js';
 export {Tokens} from './resources/Tokens.js';
 export {Topups} from './resources/Topups.js';
 export {Transfers} from './resources/Transfers.js';
-export {V2} from './resources/V2.js';
 export {WebhookEndpoints} from './resources/WebhookEndpoints.js';
 export const Apps = resourceNamespace('apps', {Secrets: AppsSecrets});
 export const Billing = resourceNamespace('billing', {
@@ -259,4 +264,16 @@ export const Treasury = resourceNamespace('treasury', {
   ReceivedDebits: TreasuryReceivedDebits,
   TransactionEntries: TreasuryTransactionEntries,
   Transactions: TreasuryTransactions,
+});
+export const V2 = resourceNamespace('v2', {
+  Billing: resourceNamespace('billing', {
+    MeterEventAdjustments: V2BillingMeterEventAdjustments,
+    MeterEventSession: V2BillingMeterEventSession,
+    MeterEventStream: V2BillingMeterEventStream,
+    MeterEvents: V2BillingMeterEvents,
+  }),
+  Core: resourceNamespace('core', {
+    EventDestinations: V2CoreEventDestinations,
+    Events: V2CoreEvents,
+  }),
 });
