bb394f1e3115e7728e7f4e9486e728243ce637de
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-11-22 14:59:13 +0000

    Update generated code for v1373

--- types/SubscriptionsResource.d.ts
+++ types/SubscriptionsResource.d.ts
@@ -1532,7 +1532,7 @@ declare module 'stripe' {
         billing_thresholds?: Stripe.Emptyable<Item.BillingThresholds>;
 
         /**
-         * Delete all usage for a given subscription item. Allowed only when `deleted` is set to `true` and the current plan's `usage_type` is `metered`.
+         * Delete all usage for a given subscription item. You must pass this when deleting a usage records subscription item. `clear_usage` has no effect if the plan has a billing meter attached.
          */
         clear_usage?: boolean;
 
