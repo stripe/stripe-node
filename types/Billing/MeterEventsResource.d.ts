3a4719cdabe37460c84130995bfedef1a838a875
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-11-26 15:12:47 +0000

    Update generated code for v1380

--- types/Billing/MeterEventsResource.d.ts
+++ types/Billing/MeterEventsResource.d.ts
@@ -22,7 +22,7 @@ declare module 'stripe' {
         expand?: Array<string>;
 
         /**
-         * A unique identifier for the event. If not provided, one will be generated. We strongly advise using UUID-like identifiers. We will enforce uniqueness within a rolling period of at least 24 hours. The enforcement of uniqueness primarily addresses issues arising from accidental retries or other problems occurring within extremely brief time intervals. This approach helps prevent duplicate entries and ensures data integrity in high-frequency operations.
+         * A unique identifier for the event. If not provided, one is generated. We recommend using UUID-like identifiers. We will enforce uniqueness within a rolling period of at least 24 hours. The enforcement of uniqueness primarily addresses issues arising from accidental retries or other problems occurring within extremely brief time intervals. This approach helps prevent duplicate entries and ensures data integrity in high-frequency operations.
          */
         identifier?: string;
 
@@ -34,7 +34,7 @@ declare module 'stripe' {
 
       class MeterEventsResource {
         /**
-         * Creates a billing meter event
+         * Creates a billing meter event.
          */
         create(
           params: MeterEventCreateParams,
