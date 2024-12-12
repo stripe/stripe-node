a695c521363e2e38ea2895dd7e319541aac5186b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-12-12 18:42:52 +0000

    Update generated code for v1405

--- types/Cards.d.ts
+++ types/Cards.d.ts
@@ -66,6 +66,11 @@ declare module 'stripe' {
       address_zip_check: string | null;
 
       /**
+       * This field indicates whether this payment method can be shown again to its customer in a checkout flow. Stripe products such as Checkout and Elements use this field to determine whether a payment method can be shown as a saved payment method in a checkout flow. The field defaults to “unspecified”.
+       */
+      allow_redisplay?: Card.AllowRedisplay | null;
+
+      /**
        * A set of available payout methods for this card. Only values from this set should be passed as the `method` when creating a payout.
        */
       available_payout_methods?: Array<Card.AvailablePayoutMethod> | null;
@@ -181,6 +186,8 @@ declare module 'stripe' {
     }
 
     namespace Card {
+      type AllowRedisplay = 'always' | 'limited' | 'unspecified';
+
       type AvailablePayoutMethod = 'instant' | 'standard';
 
       interface Networks {
