f55d9e5bd46a847388638f0b162413a94692cfaa
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-02-11 20:38:54 +0000

    Update generated code for v1483

--- types/PaymentIntents.d.ts
+++ types/PaymentIntents.d.ts
@@ -3345,14 +3345,14 @@ declare module 'stripe' {
 
       interface TransferData {
         /**
-         * Amount intended to be collected by this PaymentIntent. A positive integer representing how much to charge in the [smallest currency unit](https://stripe.com/docs/currencies#zero-decimal) (e.g., 100 cents to charge $1.00 or 100 to charge ¥100, a zero-decimal currency). The minimum amount is $0.50 US or [equivalent in charge currency](https://stripe.com/docs/currencies#minimum-and-maximum-charge-amounts). The amount value supports up to eight digits (e.g., a value of 99999999 for a USD charge of $999,999.99).
+         * The amount transferred to the destination account. This transfer will occur automatically after the payment succeeds. If no amount is specified, by default the entire payment amount is transferred to the destination account.
+         *  The amount must be less than or equal to the [amount](https://stripe.com/docs/api/payment_intents/object#payment_intent_object-amount), and must be a positive integer
+         *  representing how much to transfer in the smallest currency unit (e.g., 100 cents to charge $1.00).
          */
         amount?: number;
 
         /**
-         * The account (if any) that the payment is attributed to for tax
-         * reporting, and where funds from the payment are transferred to after
-         * payment success.
+         * The account (if any) that the payment is attributed to for tax reporting, and where funds from the payment are transferred to after payment success.
          */
         destination: string | Stripe.Account;
       }
