11912a0e1a2014f037e7fa6abef62aee07b50afe
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-02-19 17:03:54 +0000

    Update generated code for v1496

--- src/resources/TestHelpers/Terminal/Readers.ts
+++ src/resources/TestHelpers/Terminal/Readers.ts
@@ -8,4 +8,9 @@ export const Readers = StripeResource.extend({
     fullPath:
       '/v1/test_helpers/terminal/readers/{reader}/present_payment_method',
   }),
+  succeedInputCollection: stripeMethod({
+    method: 'POST',
+    fullPath:
+      '/v1/test_helpers/terminal/readers/{reader}/succeed_input_collection',
+  }),
 });
