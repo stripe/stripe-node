f5348cd57ee3bf2cfebfaad1382e253119906f89
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-02-24 19:25:00 +0000

    Update generated code for v1505

--- types/V2/Core/EventsResource.d.ts
+++ types/V2/Core/EventsResource.d.ts
@@ -12,39 +12,14 @@ declare module 'stripe' {
       namespace Core {
         interface EventListParams {
           /**
-           * Filter for events created after the specified timestamp.
+           * Primary object ID used to retrieve related events.
            */
-          created_gt?: string;
-
-          /**
-           * Filter for events created at or after the specified timestamp.
-           */
-          created_gte?: string;
-
-          /**
-           * Filter for events created before the specified timestamp.
-           */
-          created_lt?: string;
-
-          /**
-           * Filter for events created at or before the specified timestamp.
-           */
-          created_lte?: string;
-
-          /**
-           * Filter events based on whether they were successfully delivered to all subscribed event destinations. If false, events which are still pending or have failed all delivery attempts to a event destination will be returned.
-           */
-          delivery_success?: boolean;
+          object_id: string;
 
           /**
            * The page size.
            */
           limit?: number;
-
-          /**
-           * Primary object ID used to retrieve related events.
-           */
-          object_id?: string;
 
           /**
            * The requested page.
@@ -72,10 +47,9 @@ declare module 'stripe' {
            * List events, going back up to 30 days.
            */
           list(
-            params?: EventListParams,
+            params: EventListParams,
             options?: RequestOptions
           ): ApiListPromise<Stripe.V2.Event>;
-          list(options?: RequestOptions): ApiListPromise<Stripe.V2.Event>;
         }
       }
     }
