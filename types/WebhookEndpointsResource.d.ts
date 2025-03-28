7dec9f75929b8461aadad5539a6172440009def1
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-03-28 19:13:12 +0000

    Update generated code for v1638

--- types/WebhookEndpointsResource.d.ts
+++ types/WebhookEndpointsResource.d.ts
@@ -393,7 +393,14 @@ declare module 'stripe' {
         | 'treasury.received_credit.created'
         | 'treasury.received_credit.failed'
         | 'treasury.received_credit.succeeded'
-        | 'treasury.received_debit.created';
+        | 'treasury.received_debit.created'
+        | 'billing.credit_balance_transaction.created'
+        | 'billing.credit_grant.created'
+        | 'billing.credit_grant.updated'
+        | 'billing.meter.created'
+        | 'billing.meter.deactivated'
+        | 'billing.meter.reactivated'
+        | 'billing.meter.updated';
     }
 
     interface WebhookEndpointRetrieveParams {
@@ -677,7 +684,14 @@ declare module 'stripe' {
         | 'treasury.received_credit.created'
         | 'treasury.received_credit.failed'
         | 'treasury.received_credit.succeeded'
-        | 'treasury.received_debit.created';
+        | 'treasury.received_debit.created'
+        | 'billing.credit_balance_transaction.created'
+        | 'billing.credit_grant.created'
+        | 'billing.credit_grant.updated'
+        | 'billing.meter.created'
+        | 'billing.meter.deactivated'
+        | 'billing.meter.reactivated'
+        | 'billing.meter.updated';
     }
 
     interface WebhookEndpointListParams extends PaginationParams {
