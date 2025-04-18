290e730de07622bf7bec4bee486cfe67105e3804
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-04-18 22:55:08 +0000

    Update generated code for v1710

--- types/CustomersResource.d.ts
+++ types/CustomersResource.d.ts
@@ -822,7 +822,7 @@ declare module 'stripe' {
 
     interface CustomerListPaymentMethodsParams extends PaginationParams {
       /**
-       * This field indicates whether this payment method can be shown again to its customer in a checkout flow. Stripe products such as Checkout and Elements use this field to determine whether a payment method can be shown as a saved payment method in a checkout flow. The field defaults to `unspecified`.
+       * This field indicates whether this payment method can be shown again to its customer in a checkout flow. Stripe products such as Checkout and Elements use this field to determine whether a payment method can be shown as a saved payment method in a checkout flow.
        */
       allow_redisplay?: CustomerListPaymentMethodsParams.AllowRedisplay;
 
