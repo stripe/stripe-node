5233afeb86717628efcbb1014e490585c8f64b40
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-06-20 17:17:46 +0000

    Update generated code for v1816 and 2025-06-30.preview

--- types/InvoiceItems.d.ts
+++ types/InvoiceItems.d.ts
@@ -23,10 +23,7 @@ declare module 'stripe' {
     }
 
     /**
-     * Invoice Items represent the component lines of an [invoice](https://stripe.com/docs/api/invoices). An invoice item is added to an
-     * invoice by creating or updating it with an `invoice` field, at which point it will be included as
-     * [an invoice line item](https://stripe.com/docs/api/invoices/line_item) within
-     * [invoice.lines](https://stripe.com/docs/api/invoices/object#invoice_object-lines).
+     * Invoice Items represent the component lines of an [invoice](https://stripe.com/docs/api/invoices). When you create an invoice item with an `invoice` field, it is attached to the specified invoice and included as [an invoice line item](https://stripe.com/docs/api/invoices/line_item) within [invoice.lines](https://stripe.com/docs/api/invoices/object#invoice_object-lines).
      *
      * Invoice Items can be created before you are ready to actually send the invoice. This can be particularly useful when combined
      * with a [subscription](https://stripe.com/docs/api/subscriptions). Sometimes you want to add a charge or credit to a customer, but actually charge
