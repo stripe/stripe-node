f3f81f19d32bfdb2e6d30b1fa0915d93502d100a
Ramya Rao <ramya@stripe.com>
2025-07-30 10:15:14 -0700

    Bump version to 18.4.0

--- src/stripe.core.ts
+++ src/stripe.core.ts
@@ -59,7 +59,7 @@ export function createStripe(
   platformFunctions: PlatformFunctions,
   requestSender: RequestSenderFactory = defaultRequestSenderFactory
 ): typeof Stripe {
-  Stripe.PACKAGE_VERSION = '18.3.0';
+  Stripe.PACKAGE_VERSION = '18.4.0';
   Stripe.API_VERSION = ApiVersion;
   Stripe.USER_AGENT = {
     bindings_version: Stripe.PACKAGE_VERSION,
