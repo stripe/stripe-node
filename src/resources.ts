6759fd549a317f03688b5c63c8c65b197d4aca4a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-08-06 21:00:58 +0000

    Update generated code for v1881 and 2025-07-30.preview

--- src/resources.ts
+++ src/resources.ts
@@ -134,6 +134,7 @@ export {FileLinks} from './resources/FileLinks.js';
 export {Files} from './resources/Files.js';
 export {FxQuotes} from './resources/FxQuotes.js';
 export {InvoiceItems} from './resources/InvoiceItems.js';
+export {InvoicePayments} from './resources/InvoicePayments.js';
 export {InvoiceRenderingTemplates} from './resources/InvoiceRenderingTemplates.js';
 export {Invoices} from './resources/Invoices.js';
 export {Mandates} from './resources/Mandates.js';
