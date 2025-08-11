3c07eb3fd1522d7259454249ff56bb8f2f154367
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-08-11 22:58:05 +0000

    Update generated code for v1898 and 2025-07-30.preview

--- types/SetupAttempts.d.ts
+++ types/SetupAttempts.d.ts
@@ -745,6 +745,7 @@ declare module 'stripe' {
           | 'coupon_expired'
           | 'customer_max_payment_methods'
           | 'customer_max_subscriptions'
+          | 'customer_session_expired'
           | 'customer_tax_location_invalid'
           | 'debit_not_authorized'
           | 'email_invalid'
