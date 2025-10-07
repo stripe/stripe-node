98f898f22b43ee00182e561dc73eee1ff8eb046e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-10-07 16:23:43 +0000

    Update generated code for v2043 and

--- types/PaymentAttemptRecordsResource.d.ts
+++ types/PaymentAttemptRecordsResource.d.ts
@@ -24,6 +24,11 @@ declare module 'stripe' {
        * A limit on the number of objects to be returned. Limit can range between 1 and 100, and the default is 10.
        */
       limit?: number;
+
+      /**
+       * A cursor for use in pagination. `starting_after` is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with `obj_foo`, your subsequent call can include `starting_after=obj_foo` in order to fetch the next page of the list.
+       */
+      starting_after?: string;
     }
 
     class PaymentAttemptRecordsResource {
