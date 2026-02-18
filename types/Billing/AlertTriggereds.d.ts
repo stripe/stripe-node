14490f4edcb218e547dcf5894aa579609d8b390a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-02-18 23:06:29 +0000

    Update generated code for v2178 and

--- types/Billing/AlertTriggereds.d.ts
+++ types/Billing/AlertTriggereds.d.ts
@@ -13,6 +13,11 @@ declare module 'stripe' {
         object: 'billing.alert_triggered';
 
         /**
+         * The aggregation period for which this alert triggered
+         */
+        aggregation_period?: AlertTriggered.AggregationPeriod | null;
+
+        /**
          * A billing alert is a resource that notifies you when a certain usage threshold on a meter is crossed. For example, you might create a billing alert to notify you when a certain user made 100 API requests.
          */
         alert: Stripe.Billing.Alert;
@@ -43,14 +48,59 @@ declare module 'stripe' {
         external_customer_id?: string | null;
 
         /**
+         * Populated specifically for spend alerts to notify merchants which group_by entity has the exceeded spend
+         */
+        group_by?: AlertTriggered.GroupBy | null;
+
+        /**
          * Has the value `true` if the object exists in live mode or the value `false` if the object exists in test mode.
          */
         livemode: boolean;
 
         /**
+         * Timestamp at which the threshold was crossed
+         */
+        triggered_at?: number | null;
+
+        /**
          * The value triggering the alert
          */
         value: string;
+      }
+
+      namespace AlertTriggered {
+        interface AggregationPeriod {
+          /**
+           * End time of the aggregation period
+           */
+          ends_at: number;
+
+          /**
+           * Start time of the aggregation period
+           */
+          starts_at: number;
+        }
+
+        interface GroupBy {
+          /**
+           * The billing cadence ID, populated when type is `billing_cadence`
+           */
+          billing_cadence: string | null;
+
+          /**
+           * The pricing plan subscription ID, populated when type is `pricing_plan_subscription`
+           */
+          pricing_plan_subscription: string | null;
+
+          /**
+           * The type of grouping used for this alert notification
+           */
+          type: GroupBy.Type;
+        }
+
+        namespace GroupBy {
+          type Type = 'billing_cadence' | 'pricing_plan_subscription';
+        }
       }
     }
   }
