499c26d1dc576ba7ba59c1df4ed908404f7d50e4
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-05-21 00:22:59 +0000

    Update generated code for v2274 and ab03143816dc3a1903c669c355a22a6223b2161b

--- src/StripeEventNotificationHandler.ts
+++ src/StripeEventNotificationHandler.ts
@@ -149,8 +149,8 @@ export class StripeEventNotificationHandler {
 
   public async handle(
     // these types are duplicated in the manual types, so they're just here for internal use
-    rawBody: string | Buffer,
-    signature: string | Buffer
+    rawBody: string | Uint8Array,
+    signature: string | Uint8Array
   ): Promise<void> {
     // we're not worried about thread safety here because we expect callbacks will be registered synchronously on app startup
     this.hasHandledEvent = true;
