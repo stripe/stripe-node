d8f3b80f24899ebb2ae44142c3e1da650ee8f518
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-05-26 19:12:30 +0000

    Update generated code for v2277 and fab7e39871e2caacb49df35737dad10c6ca6a2ca

--- src/resources/Events.ts
+++ src/resources/Events.ts
@@ -417,6 +417,7 @@ export namespace Event {
     | 'treasury.received_debit.created'
     | 'invoice_payment.detached'
     | 'billing.alert.recovered'
+    | 'payment_intent.expired'
     | 'billing.credit_balance_transaction.created'
     | 'billing.credit_grant.updated'
     | 'billing.meter.created'
