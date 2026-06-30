fe14ddd2c1cc40ba7504e6fa7ca3b9cbc1a5316e
Jesse Rosalia <jar@stripe.com>
2026-06-30 12:29:53 -0700

    removed generated content from base

--- src/resources.ts
+++
@@ -1,1 +0,0 @@
-import {resourceNamespace} from './ResourceNamespace.js';
--- src/resources/V2/index.ts
+++
@@ -1,9 +0,0 @@
-import {DeletedObject} from './DeletedObject.js';
-import {Core} from './Core/index.js';
-
-export {DeletedObject} from './DeletedObject.js';
-
-export declare namespace V2 {
-  export {DeletedObject};
-  export {Core};
-}
