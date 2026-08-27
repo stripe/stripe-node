2f64e7ac920bd6863fdb851f4fb1fcc6190d963f
David Brownman <xavdid@stripe.com>
2026-08-26 21:14:34 -0700

    Bump version to 22.6.0

--- src/stripe.core.ts
+++ src/stripe.core.ts
@@ -963,7 +963,7 @@ const defaultRequestSenderFactory: RequestSenderFactory = (stripe) =>
   new RequestSender(stripe, StripeResource.MAX_BUFFERED_REQUEST_METRICS);
 
 export class Stripe {
-  static PACKAGE_VERSION = '22.5.0';
+  static PACKAGE_VERSION = '22.6.0';
   static API_VERSION: typeof ApiVersion = ApiVersion;
   /**
    * The major API version that this SDK uses. Objects retrieved using the same
