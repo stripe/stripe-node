9b05c2d6a9d6beefd5fbf901ec3bc557f5a29199
Mark Kogan <markisaackogan@stripe.com>
2026-09-23 11:34:41 -0400

    Drop Node's uuid4 override in platform functions

--- src/platform/NodePlatformFunctions.ts
+++ src/platform/NodePlatformFunctions.ts
@@ -20,13 +20,6 @@ class StreamProcessingError extends StripeError {}
  */
 export class NodePlatformFunctions extends PlatformFunctions {
   /** @override */
-  uuid4(): string {
-    // we need to override the base `uuid4` implementation because it uses `globalThis.crypto`, which isn't available in unflagged in node until Node 19
-    // TODO(https://go/j/DEVSDK-3253)
-    return crypto.randomUUID();
-  }
-
-  /** @override */
   getPlatformInfo(): string {
     return `${process.platform} ${os.release()} ${os.arch()}`;
   }
